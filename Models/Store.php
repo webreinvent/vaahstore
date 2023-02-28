663aea9e8b253a66cde382944044f9523ba1c71a
Bharat jat <bharatkumar-j001@webreinvent.com>
2023-02-28 19:28:03 +0530

    added Crud Boiler plate
    just added a simple crud boiler plate
    Added: basic store fields and multiselect select options
    
    added basic input field of store in vue page

--- Models/Store.php
+++ Models/Store.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Str;
 use WebReinvent\VaahCms\Traits\CrudWithUuidObservantTrait;
 use WebReinvent\VaahCms\Entities\User;
 
-class Store extends Model {
+class Store extends Model
+{
 
     use SoftDeletes;
     use CrudWithUuidObservantTrait;
@@ -35,14 +36,16 @@ class Store extends Model {
     ];
 
     //-------------------------------------------------
-    protected $appends  = [
+    protected $appends = [
     ];
+
     //-------------------------------------------------
     protected function serializeDate(DateTimeInterface $date)
     {
         $date_time_format = config('settings.global.datetime_format');
         return $date->format($date_time_format);
     }
+
     //-------------------------------------------------
 
     public function createdByUser()
@@ -67,47 +70,39 @@ class Store extends Model {
             'deleted_by', 'id'
         )->select('id', 'uuid', 'first_name', 'last_name', 'email');
     }
+
     //-------------------------------------------------
-    public function getTableColumns() {
+    public function getTableColumns()
+    {
         return $this->getConnection()->getSchemaBuilder()
             ->getColumnListing($this->getTable());
     }
+
     //-------------------------------------------------
     public function scopeExclude($query, $columns)
     {
-        return $query->select( array_diff( $this->getTableColumns(),$columns) );
+        return $query->select(array_diff($this->getTableColumns(), $columns));
     }
 
-    //-------------------------------------------------
-    public function scopeActive($query)
-    {
-        return $query->where('is_active', 1);
-    }
-    //-------------------------------------------------
-    public function scopeIsDefault($query)
-    {
-        return $query->where('is_active', 1)->where('is_default', 1);
-    }
     //-------------------------------------------------
     public function scopeBetweenDates($query, $from, $to)
     {
 
-        if($from)
-        {
+        if ($from) {
             $from = \Carbon::parse($from)
                 ->startOfDay()
                 ->toDateTimeString();
         }
 
-        if($to)
-        {
+        if ($to) {
             $to = \Carbon::parse($to)
                 ->endOfDay()
                 ->toDateTimeString();
         }
 
-        $query->whereBetween('updated_at',[$from,$to]);
+        $query->whereBetween('updated_at', [$from, $to]);
     }
+
     //-------------------------------------------------
     public static function createItem($request)
     {
@@ -115,28 +110,25 @@ class Store extends Model {
         $inputs = $request->all();
 
         $validation = self::validation($inputs);
-        if(isset($validation['failed']))
-        {
+        if (!$validation['success']) {
             return $validation;
         }
 
 
         // check if name exist
-        $item = self::where('name',$inputs['name'])->first();
+        $item = self::where('name', $inputs['name'])->withTrashed()->first();
 
-        if($item)
-        {
-            $response['failed'] = true;
+        if ($item) {
+            $response['success'] = false;
             $response['messages'][] = "This name is already exist.";
             return $response;
         }
 
         // check if slug exist
-        $item = self::where('slug',$inputs['slug'])->first();
+        $item = self::where('slug', $inputs['slug'])->withTrashed()->first();
 
-        if($item)
-        {
-            $response['failed'] = true;
+        if ($item) {
+            $response['success'] = false;
             $response['messages'][] = "This slug is already exist.";
             return $response;
         }
@@ -146,70 +138,105 @@ class Store extends Model {
         $item->slug = Str::slug($inputs['slug']);
         $item->save();
 
-        $response['success'] = true;
-        $response['data']['item'] = $item;
+        $response = self::getItem($item->id);
         $response['messages'][] = 'Saved successfully.';
         return $response;
+
+    }
+
+    //-------------------------------------------------
+    public function scopeGetSorted($query, $filter)
+    {
+
+        if(!isset($filter['sort']))
+        {
+            return $query->orderBy('id', 'desc');
+        }
+
+        $sort = $filter['sort'];
+
+
+        $direction = Str::contains($sort, ':');
+
+        if(!$direction)
+        {
+            return $query->orderBy($sort, 'asc');
+        }
+
+        $sort = explode(':', $sort);
+
+        return $query->orderBy($sort[0], $sort[1]);
+    }
+    //-------------------------------------------------
+    public function scopeIsActiveFilter($query, $filter)
+    {
+
+        if(!isset($filter['is_active'])
+            || is_null($filter['is_active'])
+            || $filter['is_active'] === 'null'
+        )
+        {
+            return $query;
+        }
+        $is_active = $filter['is_active'];
+
+        if($is_active === 'true' || $is_active === true)
+        {
+            return $query->whereNotNull('is_active');
+        } else{
+            return $query->whereNull('is_active');
+        }
+
+    }
+    //-------------------------------------------------
+    public function scopeTrashedFilter($query, $filter)
+    {
+
+        if(!isset($filter['trashed']))
+        {
+            return $query;
+        }
+        $trashed = $filter['trashed'];
+
+        if($trashed === 'include')
+        {
+            return $query->withTrashed();
+        } else if($trashed === 'only'){
+            return $query->onlyTrashed();
+        }
+
+    }
+    //-------------------------------------------------
+    public function scopeSearchFilter($query, $filter)
+    {
+
+        if(!isset($filter['q']))
+        {
+            return $query;
+        }
+        $search = $filter['q'];
+        $query->where(function ($q) use ($search) {
+            $q->where('name', 'LIKE', '%' . $search . '%')
+                ->orWhere('slug', 'LIKE', '%' . $search . '%');
+        });
 
     }
     //-------------------------------------------------
     public static function getList($request)
     {
+        $list = self::getSorted($request->filter);
+        $list->isActiveFilter($request->filter);
+        $list->trashedFilter($request->filter);
+        $list->searchFilter($request->filter);
 
+        $rows = config('vaahcms.per_page');
 
-        if(isset($request->sort))
+        if($request->has('rows'))
         {
-
-            $sort = $request->sort;
-            if(Str::contains($request->sort, ':'))
-            {
-                $sort = explode(":", $request->sort);
-                $list= self::orderBy($sort[0], $sort[1]);
-            } else
-            {
-                $list = self::orderBy($sort, 'asc');
-            }
-        } else{
-            $list = self::orderBy('id', 'desc');
+            $rows = $request->rows;
         }
 
-        if(isset($request['filter']['is_active']))
-        {
-            if($request['filter']['is_active'] == "true")
-            {
-                $list->where('is_active',1);
-            } else
-            {
-                $list->where(function ($q){
-                    $q->where('is_active',0);
-                    $q->orWhereNull('is_active');
-                });
-            }
-        }
-
-        if(isset($request['filter']['trashed']))
-        {
-            if($request['filter']['trashed'] === 'include')
-            {
-                $list->withTrashed();
-            }
-
-            if($request['filter']['trashed'] === 'only')
-            {
-                $list->onlyTrashed();
-            }
-
-        }
-
-        if(isset($request->q))
-        {
-            $list->where(function ($q) use ($request){
-                $q->where('name', 'LIKE', '%'.$request->q.'%')
-                    ->orWhere('slug', 'LIKE', '%'.$request->q.'%');
-            });
-        }
-
-        $list = $list->paginate(config('vaahcms.per_page'));
+        $list = $list->paginate($rows);
 
         $response['success'] = true;
         $response['data'] = $list;
@@ -218,6 +245,7 @@ class Store extends Model {
 
 
     }
+
     //-------------------------------------------------
     public static function updateList($request)
     {
@@ -226,33 +254,39 @@ class Store extends Model {
 
         $rules = array(
             'type' => 'required',
-            'items' => 'required',
         );
 
         $messages = array(
             'type.required' => 'Action type is required',
-            'items.required' => 'Select items',
         );
 
 
-        $validator = \Validator::make( $inputs, $rules, $messages);
-        if ( $validator->fails() ) {
+        $validator = \Validator::make($inputs, $rules, $messages);
+        if ($validator->fails()) {
 
-            $errors             = errorsToArray($validator->errors());
-            $response['failed'] = true;
+            $errors = errorsToArray($validator->errors());
+            $response['success'] = false;
             $response['errors'] = $errors;
             return $response;
         }
 
-        $items_id = collect($inputs['items'])->pluck('id')->toArray();
-
-        switch ($inputs['type'])
+        if(isset($inputs['items']))
         {
-            case 'inactive':
-                self::whereIn('id', $items_id)->update(['is_active' => null]);
+            $items_id = collect($inputs['items'])
+                ->pluck('id')
+                ->toArray();
+        }
+
+
+        $items = self::whereIn('id', $items_id)
+            ->withTrashed();
+
+        switch ($inputs['type']) {
+            case 'deactivate':
+                $items->update(['is_active' => null]);
                 break;
-            case 'active':
-                self::whereIn('id', $items_id)->update(['is_active' => 1]);
+            case 'activate':
+                $items->update(['is_active' => 1]);
                 break;
             case 'trash':
                 self::whereIn('id', $items_id)->delete();
@@ -260,7 +294,6 @@ class Store extends Model {
             case 'restore':
                 self::whereIn('id', $items_id)->restore();
                 break;
-
         }
 
         $response['success'] = true;
@@ -269,8 +302,9 @@ class Store extends Model {
 
         return $response;
     }
+
     //-------------------------------------------------
-    public static function deleteList($request)
+    public static function deleteList($request): array
     {
         $inputs = $request->all();
 
@@ -284,10 +318,10 @@ class Store extends Model {
             'items.required' => 'Select items',
         );
 
-        $validator = \Validator::make( $inputs, $rules, $messages);
-        if ( $validator->fails() ) {
+        $validator = \Validator::make($inputs, $rules, $messages);
+        if ($validator->fails()) {
 
-            $errors             = errorsToArray($validator->errors());
+            $errors = errorsToArray($validator->errors());
             $response['failed'] = true;
             $response['messages'] = $errors;
             return $response;
@@ -303,14 +337,85 @@ class Store extends Model {
         return $response;
     }
     //-------------------------------------------------
+    public static function listAction($request, $type): array
+    {
+        $inputs = $request->all();
+
+        if(isset($inputs['items']))
+        {
+            $items_id = collect($inputs['items'])
+                ->pluck('id')
+                ->toArray();
+
+            $items = self::whereIn('id', $items_id)
+                ->withTrashed();
+        }
+
+
+        switch ($type) {
+            case 'deactivate':
+                if($items->count() > 0) {
+                    $items->update(['is_active' => null]);
+                }
+                break;
+            case 'activate':
+                if($items->count() > 0) {
+                    $items->update(['is_active' => 1]);
+                }
+                break;
+            case 'trash':
+                if(isset($items_id) && count($items_id) > 0) {
+                    self::whereIn('id', $items_id)->delete();
+                }
+                break;
+            case 'restore':
+                if(isset($items_id) && count($items_id) > 0) {
+                    self::whereIn('id', $items_id)->restore();
+                }
+                break;
+            case 'delete':
+                if(isset($items_id) && count($items_id) > 0) {
+                    self::whereIn('id', $items_id)->forceDelete();
+                }
+                break;
+            case 'activate-all':
+                self::query()->update(['is_active' => 1]);
+                break;
+            case 'deactivate-all':
+                self::query()->update(['is_active' => null]);
+                break;
+            case 'trash-all':
+                self::query()->delete();
+                break;
+            case 'restore-all':
+                self::withTrashed()->restore();
+                break;
+            case 'delete-all':
+                self::withTrashed()->forceDelete();
+                break;
+        }
+
+        $response['success'] = true;
+        $response['data'] = true;
+        $response['messages'][] = 'Action was successful.';
+
+        return $response;
+    }
+    //-------------------------------------------------
     public static function getItem($id)
     {
 
         $item = self::where('id', $id)
-        ->with(['createdByUser', 'updatedByUser', 'deletedByUser'])
-        ->withTrashed()
-        ->first();
+            ->with(['createdByUser', 'updatedByUser', 'deletedByUser'])
+            ->withTrashed()
+            ->first();
 
+        if(!$item)
+        {
+            $response['success'] = false;
+            $response['errors'][] = 'Record not found with ID: '.$id;
+            return $response;
+        }
         $response['success'] = true;
         $response['data'] = $item;
 
@@ -318,83 +423,90 @@ class Store extends Model {
 
     }
     //-------------------------------------------------
-    public static function updateItem($request,$id)
+    public static function updateItem($request, $id)
     {
         $inputs = $request->all();
 
         $validation = self::validation($inputs);
-        if(isset($validation['failed']))
-        {
+        if (!$validation['success']) {
             return $validation;
         }
 
         // check if name exist
-        $user = self::where('id','!=',$inputs['id'])
-        ->where('name',$inputs['name'])->first();
+        $item = self::where('id', '!=', $inputs['id'])
+            ->withTrashed()
+            ->where('name', $inputs['name'])->first();
 
-        if($user)
-        {
-            $response['failed'] = true;
+        if ($item) {
+            $response['success'] = false;
             $response['messages'][] = "This name is already exist.";
             return $response;
         }
 
         // check if slug exist
-        $user = self::where('id','!=',$inputs['id'])
-        ->where('slug',$inputs['slug'])->first();
+        $item = self::where('id', '!=', $inputs['id'])
+            ->withTrashed()
+            ->where('slug', $inputs['slug'])->first();
 
-        if($user)
-        {
-            $response['failed'] = true;
+        if ($item) {
+            $response['success'] = false;
             $response['messages'][] = "This slug is already exist.";
             return $response;
         }
 
-        $update = self::where('id',$id)->withTrashed()->first();
-        $update->fill($inputs);
-        $update->slug = Str::slug($inputs['slug']);
-        $update->save();
+        $item = self::where('id', $id)->withTrashed()->first();
+        $item->fill($inputs);
+        $item->slug = Str::slug($inputs['slug']);
+        $item->save();
 
-        //check specific actions
-
-        if(isset($inputs['action'])){
-            switch($inputs['action'])
-            {
-                case 'trash':
-                    $update->delete();
-                    break;
-                case 'restore':
-                    $update->restore();
-                    break;
-            }
-        }
-
-
-        $response['success'] = true;
-        $response['data'] = $update;
-        $response['messages'][] = 'Record has been updated';
-
+        $response = self::getItem($item->id);
+        $response['messages'][] = 'Saved successfully.';
         return $response;
 
     }
     //-------------------------------------------------
-    public static function deleteItem($request,$id): array
+    public static function deleteItem($request, $id): array
     {
-        $update = self::where('id',$id)->withTrashed()->first();
-        if(!$update)
-        {
-            $response['failed'] = true;
+        $item = self::where('id', $id)->withTrashed()->first();
+        if (!$item) {
+            $response['success'] = false;
             $response['messages'][] = 'Record does not exist.';
             return $response;
         }
-
-        $update->forceDelete();
+        $item->forceDelete();
 
         $response['success'] = true;
         $response['data'] = [];
         $response['messages'][] = 'Record has been deleted';
 
         return $response;
+    }
+    //-------------------------------------------------
+    public static function itemAction($request, $id, $type): array
+    {
+        switch($type)
+        {
+            case 'activate':
+                self::where('id', $id)
+                    ->withTrashed()
+                    ->update(['is_active' => 1]);
+                break;
+            case 'deactivate':
+                self::where('id', $id)
+                    ->withTrashed()
+                    ->update(['is_active' => null]);
+                break;
+            case 'trash':
+                self::find($id)->delete();
+                break;
+            case 'restore':
+                self::where('id', $id)
+                    ->withTrashed()
+                    ->restore();
+                break;
+        }
+
+        return self::getItem($id);
     }
     //-------------------------------------------------
 
@@ -406,21 +518,27 @@ class Store extends Model {
             'slug' => 'required|max:150',
         );
 
-        $validator = \Validator::make( $inputs, $rules);
-        if ( $validator->fails() ) {
+        $validator = \Validator::make($inputs, $rules);
+        if ($validator->fails()) {
             $messages = $validator->errors();
-            $response['failed'] = true;
-            $response['messages'] = $messages;
+            $response['success'] = false;
+            $response['messages'] = $messages->all();
             return $response;
         }
 
+        $response['success'] = true;
+        return $response;
+
     }
+
     //-------------------------------------------------
     public static function getActiveItems()
     {
-        $item = self::where('is_active', 1)->get();
+        $item = self::where('is_active', 1)
+            ->first();
         return $item;
     }
+
     //-------------------------------------------------
     //-------------------------------------------------
     //-------------------------------------------------
---
+++ Vue/pages/store/Form.vue
@@ -0,0 +1,168 @@
+<script setup>
+import {onMounted, ref, watch} from "vue";
+import { useStoreStore } from '../../stores/store-store'
+
+import VhField from './../../vaahvue/vue-three/primeflex/VhField.vue'
+import {useRoute} from 'vue-router';
+
+
+const store = useStoreStore();
+const route = useRoute();
+
+onMounted(async () => {
+    if(route.params && route.params.id)
+    {
+        await store.getItem(route.params.id);
+    }
+
+    await store.watchItem();
+});
+
+//--------form_menu
+const form_menu = ref();
+const toggleFormMenu = (event) => {
+    form_menu.value.toggle(event);
+};
+//--------/form_menu
+
+</script>
+<template>
+
+    <div class="col-6" >
+
+        <Panel >
+
+            <template class="p-1" #header>
+
+
+                <div class="flex flex-row">
+                    <div class="p-panel-title">
+                        <span v-if="store.item && store.item.id">
+                            Update
+                        </span>
+                        <span v-else>
+                            Create
+                        </span>
+                    </div>
+
+                </div>
+
+
+            </template>
+
+            <template #icons>
+
+
+                <div class="p-inputgroup">
+                    <Button label="Save"
+                            v-if="store.item && store.item.id"
+                            data-testid="store-save"
+                            @click="store.itemAction('save')"
+                            icon="pi pi-save"/>
+
+                    <Button label="Create & New"
+                            v-else
+                            @click="store.itemAction('create-and-new')"
+                            data-testid="store-create-and-new"
+                            icon="pi pi-save"/>
+
+
+                    <!--form_menu-->
+                    <Button
+                        type="button"
+                        @click="toggleFormMenu"
+                        data-testid="store-form-menu"
+                        icon="pi pi-angle-down"
+                        aria-haspopup="true"/>
+
+                    <Menu ref="form_menu"
+                          :model="store.form_menu_list"
+                          :popup="true" />
+                    <!--/form_menu-->
+
+
+                    <Button class="p-button-primary"
+                            icon="pi pi-times"
+                            data-testid="store-to-list"
+                            @click="store.toList()">
+                    </Button>
+                </div>
+
+
+
+            </template>
+
+
+            <div v-if="store.item">
+
+                <VhField label="Name">
+                    <InputText class="w-full"
+                               name="store-name"
+                               data-testid="store-name"
+                               v-model="store.item.name"/>
+                </VhField>
+
+                <VhField label="Slug">
+                    <InputText class="w-full"
+                               name="store-slug"
+                               data-testid="store-slug"
+                               v-model="store.item.slug"/>
+                </VhField>
+
+                <VhField label="Is Multi Currency">
+                    <div class="flex flex-row">
+                        <InputSwitch v-model="store.item.is_multi_currency" />
+
+                        <div v-if="store.item.is_multi_currency" class="pl-5 w-full">
+                            <MultiSelect placeholder="Select Currencys" />
+                        </div>
+                    </div>
+                </VhField>
+
+                <VhField label="Is Multi Lingual">
+                    <div class="flex flex-row">
+                        <InputSwitch v-model="store.item.is_multi_lingual" />
+
+                        <div v-if="store.item.is_multi_lingual" class="pl-5 w-full">
+                            <MultiSelect placeholder="Select Languages" />
+                        </div>
+                    </div>
+                </VhField>
+
+                <VhField label="Is Multi Vendor">
+                    <div class="flex flex-row">
+                        <InputSwitch v-model="store.item.is_multi_vendor" />
+
+                        <div v-if="store.item.is_multi_vendor" class="pl-5 w-full">
+                            <MultiSelect placeholder="Select Languages" />
+                        </div>
+                    </div>
+                </VhField>
+
+                <VhField label="Allowed Ips">
+                    <InputSwitch v-model="store.item.allowed_ips" />
+                </VhField>
+
+                <VhField label="Is Default">
+                    <InputSwitch v-model="store.item.is_default" />
+                </VhField>
+
+                <VhField label="Status Notes">
+                    <Textarea v-model="store.item.status_notes" rows="5" cols="60" />
+                </VhField>
+
+
+                <VhField label="Is Active">
+                    <InputSwitch v-bind:false-value="0"
+                                 v-bind:true-value="1"
+                                 name="store-active"
+                                 data-testid="store-active"
+                                 v-model="store.item.is_active"/>
+                </VhField>
+
+            </div>
+        </Panel>
+
+    </div>
+
+</template>
