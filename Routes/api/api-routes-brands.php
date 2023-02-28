e9f3e576fe6ed2bde3518545d26a939ee362f41e
Tejas Mane <tejas-m001@webreinvent.com>
2023-02-28 17:01:29 +0530

    Added Crud For Brand

--- Routes/api/api-routes-brands.php
+++ Routes/api/api-routes-brands.php
@@ -1,29 +1,70 @@
 <?php
 
-
+/*
+ * API url will be: <base-url>/public/api/store/brands
+ */
 Route::group(
     [
-    'prefix' => 'store/brands',
-    'namespace' => 'Backend',
+        'prefix' => 'store/brands',
+        'namespace' => 'Backend',
     ],
-    function () {
-        //---------------------------------------------------------
-        Route::get('/assets', 'BrandsController@getAssets')
-            ->name('vh.backend.store.api.brands.assets');
-        //---------------------------------------------------------
-        Route::get('/', 'BrandsController@getList')
-            ->name('vh.backend.store.api.brands.list');
-        //---------------------------------------------------------
-        Route::post('/{id}', 'BrandsController@createItem')
-            ->name('vh.backend.store.api.brands.create');
-        //---------------------------------------------------------
-        Route::get('/{id}', 'BrandsController@getItem')
-            ->name('vh.backend.store.api.brands.read');
-        //---------------------------------------------------------
-        Route::match(['put', 'patch'], '/{id}', 'BrandsController@updateItem')
-            ->name('vh.backend.store.api.brands.update');
-        //---------------------------------------------------------
-        Route::delete('/{id}', 'BrandsController@deteleItem')
-            ->name('vh.backend.store.api.brands.delete');
-        //---------------------------------------------------------
-    });
+function () {
+
+    /**
+     * Get Assets
+     */
+    Route::get('/assets', 'BrandsController@getAssets')
+        ->name('vh.backend.store.api.brands.assets');
+    /**
+     * Get List
+     */
+    Route::get('/', 'BrandsController@getList')
+        ->name('vh.backend.store.api.brands.list');
+    /**
+     * Update List
+     */
+    Route::match(['put', 'patch'], '/', 'BrandsController@updateList')
+        ->name('vh.backend.store.api.brands.list.update');
+    /**
+     * Delete List
+     */
+    Route::delete('/', 'BrandsController@deleteList')
+        ->name('vh.backend.store.api.brands.list.delete');
+
+
+    /**
+     * Create Item
+     */
+    Route::post('/', 'BrandsController@createItem')
+        ->name('vh.backend.store.api.brands.create');
+    /**
+     * Get Item
+     */
+    Route::get('/{id}', 'BrandsController@getItem')
+        ->name('vh.backend.store.api.brands.read');
+    /**
+     * Update Item
+     */
+    Route::match(['put', 'patch'], '/{id}', 'BrandsController@updateItem')
+        ->name('vh.backend.store.api.brands.update');
+    /**
+     * Delete Item
+     */
+    Route::delete('/{id}', 'BrandsController@deleteItem')
+        ->name('vh.backend.store.api.brands.delete');
+
+    /**
+     * List Actions
+     */
+    Route::any('/action/{action}', 'BrandsController@listAction')
+        ->name('vh.backend.store.api.brands.list.action');
+
+    /**
+     * Item actions
+     */
+    Route::any('/{id}/action/{action}', 'BrandsController@itemAction')
+        ->name('vh.backend.store.api.brands.item.action');
+
+
+
+});
--- Vue/routes/routes.js
+++ Vue/routes/routes.js
@@ -1,7 +1,9 @@
 let routes= [];
 
 import dashboard from "./vue-routes-dashboard";
+import brand from "./vue-routes-brands";
 
 routes = routes.concat(dashboard);
+routes = routes.concat(brand);
 
 export default routes;
