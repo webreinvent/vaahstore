f0f201ef0cce711effaf4d596180d7bfe9d86349
Bharat jat <bharatkumar-j001@webreinvent.com>
2023-03-01 19:21:11 +0530

    Added: basic CRUD boiler plate for vendor

--- Routes/backend/routes-vendors.php
+++ Routes/backend/routes-vendors.php
@@ -2,37 +2,68 @@
 
 Route::group(
     [
-    'prefix' => 'backend/store/vendors',
-    
-    'middleware' => ['web', 'has.backend.access'],
-    
-    'namespace' => 'Backend',
+        'prefix' => 'backend/store/vendors',
+        
+        'middleware' => ['web', 'has.backend.access'],
+        
+        'namespace' => 'Backend',
 ],
 function () {
-     //---------------------------------------------------------
+    /**
+     * Get Assets
+     */
     Route::get('/assets', 'VendorsController@getAssets')
         ->name('vh.backend.store.vendors.assets');
-    //---------------------------------------------------------
+    /**
+     * Get List
+     */
     Route::get('/', 'VendorsController@getList')
         ->name('vh.backend.store.vendors.list');
-    //---------------------------------------------------------
+    /**
+     * Update List
+     */
     Route::match(['put', 'patch'], '/', 'VendorsController@updateList')
-        ->name('vh.backend.store.vendors.list.updates');
-    //---------------------------------------------------------
+        ->name('vh.backend.store.vendors.list.update');
+    /**
+     * Delete List
+     */
     Route::delete('/', 'VendorsController@deleteList')
         ->name('vh.backend.store.vendors.list.delete');
-    //---------------------------------------------------------
+
+
+    /**
+     * Create Item
+     */
     Route::post('/', 'VendorsController@createItem')
         ->name('vh.backend.store.vendors.create');
-    //---------------------------------------------------------
+    /**
+     * Get Item
+     */
     Route::get('/{id}', 'VendorsController@getItem')
         ->name('vh.backend.store.vendors.read');
-    //---------------------------------------------------------
+    /**
+     * Update Item
+     */
     Route::match(['put', 'patch'], '/{id}', 'VendorsController@updateItem')
         ->name('vh.backend.store.vendors.update');
-    //---------------------------------------------------------
+    /**
+     * Delete Item
+     */
     Route::delete('/{id}', 'VendorsController@deleteItem')
         ->name('vh.backend.store.vendors.delete');
+
+    /**
+     * List Actions
+     */
+    Route::any('/action/{action}', 'VendorsController@listAction')
+        ->name('vh.backend.store.vendors.list.actions');
+
+    /**
+     * Item actions
+     */
+    Route::any('/{id}/action/{action}', 'VendorsController@itemAction')
+        ->name('vh.backend.store.vendors.item.action');
+
     //---------------------------------------------------------
 
 });
--- Vue/routes/routes.js
+++ Vue/routes/routes.js
@@ -2,8 +2,10 @@ let routes= [];
 
 import dashboard from "./vue-routes-dashboard";
 import store from "./vue-routes-store";
+import vendor from "./vue-routes-vendors";
 
 routes = routes.concat(dashboard);
 routes = routes.concat(store);
+routes = routes.concat(vendor);
 
 export default routes;
