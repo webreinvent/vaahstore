6db8392d1b0985601a3bf1c4c1bcd3098a822589
Bharat jat <bharatkumar-j001@webreinvent.com>
2023-03-03 13:50:06 +0530

    Added: Owned By in vendor

---
+++ Database/Migrations/2023_03_02_180845_vh_st_vendor_users.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class VhStvendorusers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        if (!Schema::hasTable('vh_st_vendor_users')) {
+            Schema::create('vh_st_vendor_users', function (Blueprint $table) {
+                $table->bigIncrements('id')->unsigned();
+                $table->uuid('uuid')->nullable()->index();
+
+                $table->integer('vh_st_vendor_id')->nullable()->index();
+                $table->integer('vh_user_id')->nullable()->index();
+                $table->integer('vh_role_id')->nullable()->index();
+
+
+                $table->string('name')->nullable()->index();
+                $table->string('slug')->nullable()->index();
+                $table->boolean('is_active')->nullable()->index();
+                $table->string('status')->nullable()->index();
+                $table->string('status_notes')->nullable();
+
+                //----common fields
+                $table->text('meta')->nullable();
+                $table->bigInteger('created_by')->nullable()->index();
+                $table->bigInteger('updated_by')->nullable()->index();
+                $table->bigInteger('deleted_by')->nullable()->index();
+                $table->timestamps();
+                $table->softDeletes();
+                $table->index(['created_at', 'updated_at', 'deleted_at']);
+                //----/common fields
+
+            });
+        }
+
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::dropIfExists('vh_st_vendor_users');
+    }
+}
