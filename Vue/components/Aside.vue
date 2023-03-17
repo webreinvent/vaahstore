b50c98ce2ac6f5daa15c18bf92d8613b6031f655
Tejas Mane <tejas-m001@webreinvent.com>
2023-03-17 16:53:41 +0530

    Update: conflict

--- Vue/components/Aside.vue
+++ Vue/components/Aside.vue
@@ -42,23 +42,8 @@ const items = ref([
             {
                 label: 'Brands',
                 icon: 'fa-regular fa-chart-bar',
-                to: "/vendors"
-            },
-            {
-                label: 'Product',
-                icon: 'fa-regular fa-chart-bar',
-                to: "/products"
-            },
-            {
-                label: 'Product-Variations',
-                icon: 'fa-regular fa-chart-bar',
-                to: "/productvariations"
-            },
-            {
-                label: 'Brands',
-                icon: 'fa-regular fa-chart-bar',
                 to: "/brands"
-            }
+            },
         ]
     },
 ]);
