b9d32e2b10968a298159e0e747da302bc8351aa2
Tejas Mane <tejas-m001@webreinvent.com>
2023-03-17 17:40:09 +0530

    Added: Curd for product_medias

---
+++ Http/Controllers/Backend/ProductMediasController.php
@@ -0,0 +1,227 @@
+<?php namespace VaahCms\Modules\Store\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use VaahCms\Modules\Store\Models\ProductMedia;
+
+
+class ProductMediasController extends Controller
+{
+
+
+    //----------------------------------------------------------
+    public function __construct()
+    {
+
+    }
+
+    //----------------------------------------------------------
+
+    public function getAssets(Request $request)
+    {
+
+        try{
+
+            $data = [];
+
+            $data['permission'] = [];
+            $data['rows'] = config('vaahcms.per_page');
+
+            $data['fillable']['except'] = [
+                'uuid',
+                'created_by',
+                'updated_by',
+                'deleted_by',
+            ];
+
+            $model = new ProductMedia();
+            $fillable = $model->getFillable();
+            $data['fillable']['columns'] = array_diff(
+                $fillable, $data['fillable']['except']
+            );
+
+            foreach ($fillable as $column)
+            {
+                $data['empty_item'][$column] = null;
+            }
+
+            $data['actions'] = [];
+
+            $response['success'] = true;
+            $response['data'] = $data;
+
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+        }
+
+        return $response;
+    }
+
+    //----------------------------------------------------------
+    public function getList(Request $request)
+    {
+        try{
+            return ProductMedia::getList($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function updateList(Request $request)
+    {
+        try{
+            return ProductMedia::updateList($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function listAction(Request $request, $type)
+    {
+
+
+        try{
+            return ProductMedia::listAction($request, $type);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+
+        }
+    }
+    //----------------------------------------------------------
+    public function deleteList(Request $request)
+    {
+        try{
+            return ProductMedia::deleteList($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function createItem(Request $request)
+    {
+        try{
+            return ProductMedia::createItem($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function getItem(Request $request, $id)
+    {
+        try{
+            return ProductMedia::getItem($id);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function updateItem(Request $request,$id)
+    {
+        try{
+            return ProductMedia::updateItem($request,$id);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function deleteItem(Request $request,$id)
+    {
+        try{
+            return ProductMedia::deleteItem($request,$id);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function itemAction(Request $request,$id,$action)
+    {
+        try{
+            return ProductMedia::itemAction($request,$id,$action);
+        }catch (\Exception $e){
+            $response = [];
+            $response['status'] = 'failed';
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+
+
+}
