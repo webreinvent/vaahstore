a5fd6098870ac8693804cd5d3b9b46cd89625005
Bharat jat <bharatkumar-j001@webreinvent.com>
2023-03-20 16:24:03 +0530

    Added: functionality to save and update in product stock

--- Vue/pages/productstocks/Item.vue
+++ Vue/pages/productstocks/Item.vue
@@ -137,7 +137,7 @@ const toggleItemMenu = (event) => {
                     <tbody class="p-datatable-tbody">
                     <template v-for="(value, column) in store.item ">
 
-                        <template v-if="column === 'created_by' || column === 'updated_by'">
+                        <template v-if="column === 'created_by' || column === 'updated_by' || column === 'status' || column === 'vendor' || column === 'product' || column === 'product_variation' || column === 'warehouse'">
                         </template>
 
                         <template v-else-if="column === 'id' || column === 'uuid'">
@@ -158,6 +158,41 @@ const toggleItemMenu = (event) => {
                             <VhViewRow :label="column"
                                        :value="value"
                                        type="yes-no"
+                            />
+                        </template>
+
+                        <template v-else-if="column === 'taxonomy_id_product_stock_status'">
+                            <VhViewRow label="Status"
+                                       :value="value"
+                                       type="status"
+                            />
+                        </template>
+
+                        <template v-else-if="column === 'vh_st_vendor_id'">
+                            <VhViewRow label="Vendor"
+                                       :value="value"
+                                       type="user"
+                            />
+                        </template>
+
+                        <template v-else-if="column === 'vh_st_product_id'">
+                            <VhViewRow label="Product"
+                                       :value="value"
+                                       type="user"
+                            />
+                        </template>
+
+                        <template v-else-if="column === 'vh_st_product_variation_id'">
+                            <VhViewRow label="Product Variation"
+                                       :value="value"
+                                       type="user"
+                            />
+                        </template>
+
+                        <template v-else-if="column === 'vh_st_warehouse_id'">
+                            <VhViewRow label="Product Variation"
+                                       :value="value"
+                                       type="user"
                             />
                         </template>
 
