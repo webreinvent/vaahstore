a28f333b539e1759da566d41dbb4023509100285
Tejas Mane <tejas-m001@webreinvent.com>
2023-03-29 11:11:10 +0530

    Added: crud for store payment method with seed

---
+++ Database/Migrations/2023_03_29_105917_vh_st_store_payment_methods.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class VhStstorepaymentmethods extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        if (!Schema::hasTable('vh_st_store_payment_methods')) {
+            Schema::create('vh_st_store_payment_methods', function (Blueprint $table) {
+                $table->bigIncrements('id')->unsigned();
+                $table->uuid('uuid')->nullable()->index();
+                $table->integer('vh_st_store_id')->nullable()->index();
+                $table->integer('vh_st_payment_method_id')->nullable()->index();
+                $table->integer('taxonomy_id_payment_methods_status')->nullable()->index();
+
+                $table->dateTime('last_payment_at')->nullable();
+
+                $table->string('name')->nullable()->index();
+                $table->string('slug')->nullable()->index();
+                $table->boolean('is_active')->nullable()->index();
+
+
+                //----common fields
+                $table->string('status_notes')->nullable()->index();
+                $table->text('meta')->nullable();
+                $table->bigInteger('created_by')->nullable()->index();
+                $table->bigInteger('updated_by')->nullable()->index();
+                $table->bigInteger('deleted_by')->nullable()->index();
+                $table->timestamps();
+                $table->softDeletes();
+                $table->index(['created_at', 'updated_at', 'deleted_at']);
+                //----/common fields
+
+            });
+        }
+
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::dropIfExists('vh_st_store_payment_methods');
+    }
+}
---
+++ Http/Controllers/Backend/StorePaymentMethodsController.php
@@ -0,0 +1,227 @@
+<?php namespace VaahCms\Modules\Store\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use VaahCms\Modules\Store\Models\StorePaymentMethod;
+
+
+class StorePaymentMethodsController extends Controller
+{
+
+
+    //----------------------------------------------------------
+    public function __construct()
+    {
+
+    }
+
+    //----------------------------------------------------------
+
+    public function getAssets(Request $request)
+    {
+
+        try{
+
+            $data = [];
+
+            $data['permission'] = [];
+            $data['rows'] = config('vaahcms.per_page');
+
+            $data['fillable']['except'] = [
+                'uuid',
+                'created_by',
+                'updated_by',
+                'deleted_by',
+            ];
+
+            $model = new StorePaymentMethod();
+            $fillable = $model->getFillable();
+            $data['fillable']['columns'] = array_diff(
+                $fillable, $data['fillable']['except']
+            );
+
+            foreach ($fillable as $column)
+            {
+                $data['empty_item'][$column] = null;
+            }
+
+            $data['actions'] = [];
+
+            $response['success'] = true;
+            $response['data'] = $data;
+
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+        }
+
+        return $response;
+    }
+
+    //----------------------------------------------------------
+    public function getList(Request $request)
+    {
+        try{
+            return StorePaymentMethod::getList($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function updateList(Request $request)
+    {
+        try{
+            return StorePaymentMethod::updateList($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function listAction(Request $request, $type)
+    {
+
+
+        try{
+            return StorePaymentMethod::listAction($request, $type);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+
+        }
+    }
+    //----------------------------------------------------------
+    public function deleteList(Request $request)
+    {
+        try{
+            return StorePaymentMethod::deleteList($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function createItem(Request $request)
+    {
+        try{
+            return StorePaymentMethod::createItem($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function getItem(Request $request, $id)
+    {
+        try{
+            return StorePaymentMethod::getItem($id);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function updateItem(Request $request,$id)
+    {
+        try{
+            return StorePaymentMethod::updateItem($request,$id);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function deleteItem(Request $request,$id)
+    {
+        try{
+            return StorePaymentMethod::deleteItem($request,$id);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function itemAction(Request $request,$id,$action)
+    {
+        try{
+            return StorePaymentMethod::itemAction($request,$id,$action);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+
+
+}
