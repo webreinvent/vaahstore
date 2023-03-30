396229da538a315c51c3a6830581b046d29cee81
Tejas Mane <tejas-m001@webreinvent.com>
2023-03-30 13:16:09 +0530

    Update conflict

--- Vue/components/Aside.vue
+++ Vue/components/Aside.vue
@@ -92,7 +92,7 @@ const items = ref([
 <template>
 
     <div v-if="height">
-        <Menu style="width: 300px" :model="items" />
+        <Menu class="col-10" :model="items" />
     </div>
 
 </template>
