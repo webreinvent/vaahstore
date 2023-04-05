047920998476c80378dbcb4197252e082660daa6
Tejas Mane <tejas-m001@webreinvent.com>
2023-04-05 18:12:30 +0530

    Added: migration

--- Database/Migrations/2023_04_05_115946_vh_st_media_images.php
+++ Database/Migrations/2023_04_05_115946_vh_st_media_images.php
@@ -12,8 +12,26 @@ class VhStMediaImages extends Migration
      * @return void
      */
     public function up()
-    {
-        //
+    { if (!Schema::hasTable('vh_st_media_images')) {
+        Schema::create('vh_st_media_images', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->uuid('uuid')->nullable()->index();
+            $table->integer('vh_st_product_id')->nullable()->index();
+            $table->string('image_path')->nullable()->index();
+
+            //----common fields
+            $table->text('meta')->nullable();
+            $table->bigInteger('created_by')->nullable()->index();
+            $table->bigInteger('updated_by')->nullable()->index();
+            $table->bigInteger('deleted_by')->nullable()->index();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->index(['created_at', 'updated_at', 'deleted_at']);
+            //----/common fields
+
+        });
+    }
+
     }
 
     /**
@@ -23,6 +41,6 @@ class VhStMediaImages extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('vh_st_media_images');
     }
 }
