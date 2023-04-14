ab8c775fa7e63c239eb2acd6795bee91dbd656d2
Bharat jat <bharatkumar-j001@webreinvent.com>
2023-04-14 17:41:59 +0530

    update: migration for image to image_url

--- Database/Migrations/2023_03_17_173857_vh_st_product_medias.php
+++ Database/Migrations/2023_03_17_173857_vh_st_product_medias.php
@@ -23,9 +23,9 @@ class VhStproductmedias extends Migration
                 $table->integer('taxonomy_id_product_media_status')->nullable()->index();
 
                 $table->string('status_notes')->nullable();
-                $table->string('image')->nullable();
+                $table->string('image_url')->nullable();
                 $table->bigInteger('image_size')->nullable()->index();
-                $table->string('thumbnail')->nullable();
+                $table->string('thumbnail_url')->nullable();
                 $table->bigInteger('thumbnail_size')->nullable()->index();
                 $table->string('url')->nullable();
                 $table->boolean('is_active')->nullable()->index();
