12a36d96088301f572c2c0a622fdf31b78b53340
Bharat jat <bharatkumar-j001@webreinvent.com>
2023-04-20 15:30:38 +0530

    Update: handling of currency and languages methods

--- Http/Controllers/Backend/StoresController.php
+++ Http/Controllers/Backend/StoresController.php
@@ -52,8 +52,9 @@ class StoresController extends Controller
         }
 
         $data['taxonomy']['status'] = Taxonomy::getTaxonomyByType('store-status');
-        $data['currencies'] = VaahCountry::getListWithCurrency();
-        $data['languages'] = VaahCountry::getListWithLanguage();
+
+        $data['currencies'] = $this->getCurrencies();
+        $data['languages'] = $this->getLanguages();
 
         $data['empty_item']['is_multi_currency'] = 0;
         $data['empty_item']['is_multi_lingual'] = 0;
@@ -67,6 +68,42 @@ class StoresController extends Controller
 
         return $response;
     }
+
+    //----------------------------------------------------------
+    public function getCurrencies()
+    {
+        if (method_exists('VaahCountry', 'getListWithCurrency')){
+            return VaahCountry::getListWithCurrency();
+        }else{
+            return array(
+                array("name" => "Australian Dollar", "code" => "AUD", "symbol" => "$"),
+                array("name" => "Barbadian Dollar", "code" => "BBD", "symbol" => "Bds$"),
+                array("name" => "Bitcoin", "code" => "BTC", "symbol" => "฿"),
+                array("name" => "Cambodian Riel", "code" => "KHR", "symbol" => "KHR"),
+                array("name" => "Danish Krone", "code" => "DKK", "symbol" => "Kr."),
+                array("name" => "Indian Rupee", "code" => "INR", "symbol" => "₹")
+            );
+        }
+    }
+
+    //----------------------------------------------------------
+    public function getLanguages()
+    {
+        if (method_exists('VaahCountry', 'getListWithLanguage')){
+            return VaahCountry::getListWithCurrency();
+        }else{
+            return array(
+                array("name" => "Acoli"),
+                array("name" => "Adangme"),
+                array("name" => "Adyghe; Adygei"),
+                array("name" => "Blin; Bilin"),
+                array("name" => "Western Frisian"),
+                array("name" => "Gbaya"),
+                array("name" => "Haida")
+            );
+        }
+    }
+
     //----------------------------------------------------------
     public function getList(Request $request)
     {
