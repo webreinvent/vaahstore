9027126c6bf9a461da9b9fa8a7826f6629fda3cd
Tejas Mane <tejas-m001@webreinvent.com>
2023-04-20 12:18:12 +0530

    Update: migration indexing

--- Database/Migrations/2023_03_29_105917_vh_st_store_payment_methods.php
+++ Database/Migrations/2023_03_29_105917_vh_st_store_payment_methods.php
@@ -20,7 +20,7 @@ class VhStstorepaymentmethods extends Migration
                 $table->uuid('uuid')->nullable()->index();
                 $table->integer('vh_st_store_id')->nullable()->index();
                 $table->integer('vh_st_payment_method_id')->nullable()->index();
-                $table->integer('taxonomy_id_payment_methods_status')->nullable()->index();
+                $table->integer('taxonomy_id_payment_status')->nullable()->index();
 
                 $table->dateTime('last_payment_at')->nullable();
 
@@ -37,7 +37,9 @@ class VhStstorepaymentmethods extends Migration
                 $table->bigInteger('deleted_by')->nullable()->index();
                 $table->timestamps();
                 $table->softDeletes();
-                $table->index(['created_at', 'updated_at', 'deleted_at']);
+                $table->index('created_at');
+                $table->index('updated_at');
+                $table->index('deleted_at');
                 //----/common fields
 
             });
