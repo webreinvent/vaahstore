eef3b172fb6a664c5598b25f5df1fff68186566c
Suraj Kumar <suraj-k001@webreinvent.com>
2023-10-04 19:31:36 +0530

    Updated : Added required files for warehouse crud

--- Http/Controllers/Backend/WarehousesController.php
+++ Http/Controllers/Backend/WarehousesController.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use VaahCms\Modules\Store\Models\Vendor;
 use VaahCms\Modules\Store\Models\Warehouse;
+use VaahCms\Modules\Store\Models\Vendor;
 use WebReinvent\VaahCms\Entities\Taxonomy;
 use WebReinvent\VaahExtend\Facades\VaahCountry;
-
-
 class WarehousesController extends Controller
 {
 
@@ -30,42 +28,26 @@ class WarehousesController extends Controller
             $data['permission'] = [];
             $data['rows'] = config('vaahcms.per_page');
 
-            $data['fillable']['except'] = [
-                'uuid',
-                'created_by',
-                'updated_by',
-                'deleted_by',
-            ];
-
-            $model = new Warehouse();
-            $fillable = $model->getFillable();
-            $data['fillable']['columns'] = array_diff(
-                $fillable, $data['fillable']['except']
-            );
-
-            foreach ($fillable as $column)
-            {
-                $data['empty_item'][$column] = null;
-            }
-
+            $data['fillable']['columns'] = Warehouse::getFillableColumns();
+            $data['fillable']['except'] = Warehouse::getUnFillableColumns();
+            $data['empty_item'] = Warehouse::getEmptyItem();
             $data['taxonomy']['status'] = Taxonomy::getTaxonomyByType('warehouse-status');
             $data['countries'] = array_column(VaahCountry::getList(), 'name');
-
             $data['empty_item']['is_active'] = 1;
             $data['empty_item']['vendor'] = null;
             $data['empty_item']['status'] = null;
-            $data['actions'] = [];
-
             $get_vendor_data = self::getVendorData();
             $data['empty_item']['vh_st_vendor_id'] = $this->getDefaultRow($get_vendor_data['vendors']) ?? null;
             $data = array_merge($data, $get_vendor_data);
+
+            $data['actions'] = [];
 
             $response['success'] = true;
             $response['data'] = $data;
 
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -76,7 +58,10 @@ class WarehousesController extends Controller
 
         return $response;
     }
-    //---------------------Get Default Data-------------------------------------
+
+
+    //---------------------Get Default Data----------------------
+
     public function getDefaultRow($row){
         foreach($row as $k=>$v)
         {
@@ -86,7 +71,9 @@ class WarehousesController extends Controller
             }
         }
     }
-    //------------------------Get Vendor data for dropdown----------------------------------
+
+    //----------------------------------------------------------
+
     public function getVendorData(){
         try{
             $data['vendors'] = Vendor::where('is_active',1)->get();
@@ -103,14 +90,16 @@ class WarehousesController extends Controller
             }
         }
     }
+
     //----------------------------------------------------------
+
     public function getList(Request $request)
     {
         try{
             return Warehouse::getList($request);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -127,7 +116,7 @@ class WarehousesController extends Controller
             return Warehouse::updateList($request);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -147,7 +136,7 @@ class WarehousesController extends Controller
             return Warehouse::listAction($request, $type);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -165,7 +154,24 @@ class WarehousesController extends Controller
             return Warehouse::deleteList($request);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+            }
+            return $response;
+        }
+    }
+    //----------------------------------------------------------
+    public function fillItem(Request $request)
+    {
+        try{
+            return Warehouse::fillItem($request);
+        }catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -182,7 +188,7 @@ class WarehousesController extends Controller
             return Warehouse::createItem($request);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -199,7 +205,7 @@ class WarehousesController extends Controller
             return Warehouse::getItem($id);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -216,7 +222,7 @@ class WarehousesController extends Controller
             return Warehouse::updateItem($request,$id);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -233,7 +239,7 @@ class WarehousesController extends Controller
             return Warehouse::deleteItem($request,$id);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
@@ -250,7 +256,7 @@ class WarehousesController extends Controller
             return Warehouse::itemAction($request,$id,$action);
         }catch (\Exception $e){
             $response = [];
-            $response['status'] = 'failed';
+            $response['success'] = false;
             if(env('APP_DEBUG')){
                 $response['errors'][] = $e->getMessage();
                 $response['hint'] = $e->getTrace();
