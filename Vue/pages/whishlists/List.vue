ad3cca5e4c0550e3b60b926868d397003550aa81
anil-y001 <anil-y001@webreinvent.com>
2023-10-06 16:30:48 +0530

    Changed: name whishlists to wishlist

--- Vue/pages/whishlists/List.vue
+++ Vue/pages/whishlists/List.vue
@@ -71,7 +71,7 @@ const toggleCreateMenu = (event) => {
 
                     <div class="flex flex-row">
                         <div >
-                            <b class="mr-1">Whishlists</b>
+                            <b class="mr-1">Wishlist</b>
                             <Badge v-if="store.list && store.list.total > 0"
                                    :value="store.list.total">
                             </Badge>
