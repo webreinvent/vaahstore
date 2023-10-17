f60e24e67b043fe650d575d98ab8a7544a333ff1
Suraj Kumar <suraj-k001@webreinvent.com>
2023-10-17 19:30:35 +0530

    Added : build files and updated composer.json and config files

--- Config/config.php
+++ Config/config.php
@@ -11,7 +11,7 @@ return [
     "download_link"=> "",
     "author_name"=> "store",
     "author_website"=> "https://vaah.dev",
-    "version"=> "0.0.33",
+    "version"=> "0.0.32",
     "is_migratable"=> true,
     "is_sample_data_available"=> true,
     "db_table_prefix"=> "vh_store_",
