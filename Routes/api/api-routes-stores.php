5c28024342932582d0df738002fa83356a9dbe0a
Suraj Kumar <suraj-k001@webreinvent.com>
2023-10-23 11:30:57 +0530

    updated : all the files of Store-CRUD

--- Routes/api/api-routes-stores.php
+++ Routes/api/api-routes-stores.php
@@ -1,75 +1,70 @@
 <?php
 
+/*
+ * API url will be: <base-url>/public/api/store/stores
+ */
 Route::group(
     [
-        'prefix' => 'backend/store/stores',
-        
-        'middleware' => ['web', 'has.backend.access'],
-        
+        'prefix' => 'store/stores',
         'namespace' => 'Backend',
-],
+    ],
 function () {
+
     /**
      * Get Assets
      */
     Route::get('/assets', 'StoresController@getAssets')
-        ->name('vh.backend.store.stores.assets');
+        ->name('vh.backend.store.api.stores.assets');
     /**
      * Get List
      */
     Route::get('/', 'StoresController@getList')
-        ->name('vh.backend.store.stores.list');
+        ->name('vh.backend.store.api.stores.list');
     /**
      * Update List
      */
     Route::match(['put', 'patch'], '/', 'StoresController@updateList')
-        ->name('vh.backend.store.stores.list.update');
+        ->name('vh.backend.store.api.stores.list.update');
     /**
      * Delete List
      */
     Route::delete('/', 'StoresController@deleteList')
-        ->name('vh.backend.store.stores.list.delete');
+        ->name('vh.backend.store.api.stores.list.delete');
 
-
-    /**
-     * Fill Form Inputs
-     */
-    Route::any('/fill', 'StoresController@fillItem')
-        ->name('vh.backend.store.stores.fill');
 
     /**
      * Create Item
      */
     Route::post('/', 'StoresController@createItem')
-        ->name('vh.backend.store.stores.create');
+        ->name('vh.backend.store.api.stores.create');
     /**
      * Get Item
      */
     Route::get('/{id}', 'StoresController@getItem')
-        ->name('vh.backend.store.stores.read');
+        ->name('vh.backend.store.api.stores.read');
     /**
      * Update Item
      */
     Route::match(['put', 'patch'], '/{id}', 'StoresController@updateItem')
-        ->name('vh.backend.store.stores.update');
+        ->name('vh.backend.store.api.stores.update');
     /**
      * Delete Item
      */
     Route::delete('/{id}', 'StoresController@deleteItem')
-        ->name('vh.backend.store.stores.delete');
+        ->name('vh.backend.store.api.stores.delete');
 
     /**
      * List Actions
      */
     Route::any('/action/{action}', 'StoresController@listAction')
-        ->name('vh.backend.store.stores.list.actions');
+        ->name('vh.backend.store.api.stores.list.action');
 
     /**
      * Item actions
      */
     Route::any('/{id}/action/{action}', 'StoresController@itemAction')
-        ->name('vh.backend.store.stores.item.action');
+        ->name('vh.backend.store.api.stores.item.action');
 
-    //---------------------------------------------------------
+
 
 });
