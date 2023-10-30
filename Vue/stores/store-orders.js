a6a0133fb9c4b7b38ee83b88a990cebcd7b0b266
Suraj Kumar <suraj-k001@webreinvent.com>
2023-10-30 17:07:38 +0530

    updated : orders crud

--- Vue/stores/store-orders.js
+++ Vue/stores/store-orders.js
@@ -266,7 +266,7 @@ export const useOrderStore = defineStore({
             this.watch_stopper = watch(route, (newVal,oldVal) =>
                 {
 
-                    if(this.watch_stopper && !newVal.name.includes(this.route_prefix)){
+                    if(this.watch_stopper && !newVal.name.startsWith(this.route_prefix)){
                         this.watch_stopper();
 
                         return false;
