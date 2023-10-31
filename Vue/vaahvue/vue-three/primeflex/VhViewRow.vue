5aecbb7ce9fc9c5904bc802a2ebbdd50e511983d
Suraj Kumar <suraj-k001@webreinvent.com>
2023-10-31 14:23:51 +0530

    updated : orders-crud, added model file

--- Vue/vaahvue/vue-three/primeflex/VhViewRow.vue
+++ Vue/vaahvue/vue-three/primeflex/VhViewRow.vue
@@ -50,13 +50,13 @@ const props = defineProps({
         <template v-else-if="type==='status'">
             <td colspan="2" >
                 <template v-if="typeof value === 'object' && value !== null">
-                    <Badge v-if="value.slug == 'approved'"  @click="vaah().copy(value.name)" severity="success">
+                    <Tag v-if="value.slug == 'approved'"  @click="vaah().copy(value.name)" severity="success">
                         {{value.name}}
-                    </Badge>
-                    <Badge v-else-if="value.slug == 'pending'" @click="vaah().copy(value.name)" severity="warning">
+                    </Tag>
+                    <Tag v-else-if="value.slug == 'pending'" @click="vaah().copy(value.name)" severity="warning">
                         {{value.name}}
-                    </Badge>
-                    <Badge v-else-if="value.slug == 'rejected'" @click="vaah().copy(value.name)" severity="danger">
+                    </Tag>
+                    <Tag v-else-if="value.slug == 'rejected'" @click="vaah().copy(value.name)" severity="danger">
                         {{value.name}}
                     </Badge>
                     <Badge v-else @click="vaah().copy(value.id)" severity="primary">
