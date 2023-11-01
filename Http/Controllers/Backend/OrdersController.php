dea76ede91460a8889db3015ca8104ca2eac6723
Suraj Kumar <suraj-k001@webreinvent.com>
2023-11-01 15:15:28 +0530

    updated : autocomplete section of order item vue

--- Http/Controllers/Backend/OrdersController.php
+++ Http/Controllers/Backend/OrdersController.php
@@ -254,6 +254,53 @@ class OrdersController extends Controller
 
     //----------------------------------------------------------
 
+    public function searchProductVariations(Request $request)
+    {
+        try {
+
+            return Order::searchProductVariations($request);
+        }
+        catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+
+            }
+            return $response;
+        }
+
+    }
+
+    //----------------------------------------------------------
+
+    public function searchVendor(Request $request)
+    {
+        try {
+
+            return Order::searchVendor($request);
+        }
+        catch (\Exception $e){
+            $response = [];
+            $response['success'] = false;
+            if(env('APP_DEBUG')){
+                $response['errors'][] = $e->getMessage();
+                $response['hint'] = $e->getTrace();
+            } else{
+                $response['errors'][] = 'Something went wrong.';
+
+            }
+            return $response;
+        }
+
+    }
+
+    //----------------------------------------------------------
+
+
     public function fillItem(Request $request)
     {
         try {
