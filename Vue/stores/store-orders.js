d03c4b4959fb4aa17655566ca47f300d5cbf4952
Suraj Kumar <suraj-k001@webreinvent.com>
2023-11-01 16:40:59 +0530

    updated : orders-crud

--- Vue/stores/store-orders.js
+++ Vue/stores/store-orders.js
@@ -79,6 +79,7 @@ export const useOrderStore = defineStore({
         filtered_product_variations : null,
         filtered_venders :null,
         vendors : null,
+        filtered_customer_groups : null,
     }),
     getters: {
 
@@ -122,15 +123,6 @@ export const useOrderStore = defineStore({
 
         //---------------------------------------------------------------------
 
-        searchCustomerGroup(event) {
-
-            this.customer_group_suggestion= this.customer_groups.filter((customer_groups) => {
-                return customer_groups.name.toLowerCase().startsWith(event.query.toLowerCase());
-            });
-
-        },
-        //---------------------------------------------------------------------
-
         async searchProduct(event)
         {
             const query = {
@@ -144,7 +136,7 @@ export const useOrderStore = defineStore({
                 method: 'post',
             };
             await vaah().ajax(
-                this.ajax_url+'/search/products',
+                this.ajax_url+'/search/product',
                 this.searchProductAfter,
                 options
             );
@@ -175,7 +167,7 @@ export const useOrderStore = defineStore({
                 method: 'post',
             };
             await vaah().ajax(
-                this.ajax_url+'/search/product-variations',
+                this.ajax_url+'/search/product-variation',
                 this.searchProductVariationAfter,
                 options
             );
@@ -223,7 +215,7 @@ export const useOrderStore = defineStore({
 
         //---------------------------------------------------------------------
 
-        async searchCustomerGroups(event) {
+        async searchCustomerGroup(event) {
 
             const query = {
                 filter: {
@@ -236,8 +228,8 @@ export const useOrderStore = defineStore({
                 method: 'post',
             };
             await vaah().ajax(
-                this.ajax_url+'/search/vendor',
-                this.searchCustomerGroupsAfter,
+                this.ajax_url+'/search/customer-group',
+                this.searchCustomerGroupAfter,
                 options
             );
 
@@ -245,17 +237,18 @@ export const useOrderStore = defineStore({
 
         //---------------------------------------------------------------------
 
-        searchCustomerGroupsAfter(data,res) {
+        searchCustomerGroupAfter(data,res) {
+
             if(data)
             {
-                this.filtered_venders = data;
+                this.filtered_customer_groups = data;
+                console.log(data);
             }
-        },
 
+        },
 
         //---------------------------------------------------------------------
 
-        
         searchStatus(event) {
 
             this.status_suggestion= this.status_orders.filter((status_orders) => {
@@ -792,6 +785,7 @@ export const useOrderStore = defineStore({
             await this.getList();
         },
         //---------------------------------------------------------------------
+        
         async getFormInputs () {
             let params = {
                 model_namespace: this.model,
