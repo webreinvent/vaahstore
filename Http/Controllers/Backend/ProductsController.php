a34b83183e7fc2ccf095ff186efc7981c6f155a4
Suraj Kumar <suraj-k001@webreinvent.com>
2023-11-06 16:23:11 +0530

    updated : all the files of products crud

--- Http/Controllers/Backend/ProductsController.php
+++ Http/Controllers/Backend/ProductsController.php
@@ -498,52 +498,6 @@ class ProductsController extends Controller
     }
     //----------------------------------------------------------
 
-    public function searchStore(Request $request)
-    {
-        try {
-
-            return Product::searchStore($request);
-        }
-        catch (\Exception $e){
-            $response = [];
-            $response['success'] = false;
-            if(env('APP_DEBUG')){
-                $response['errors'][] = $e->getMessage();
-                $response['hint'] = $e->getTrace();
-            } else{
-                $response['errors'][] = 'Something went wrong.';
-
-            }
-            return $response;
-        }
-
-    }
-
-    //----------------------------------------------------------
-
-    public function searchBrand(Request $request)
-    {
-        try {
-
-            return Product::searchBrand($request);
-        }
-        catch (\Exception $e){
-            $response = [];
-            $response['success'] = false;
-            if(env('APP_DEBUG')){
-                $response['errors'][] = $e->getMessage();
-                $response['hint'] = $e->getTrace();
-            } else{
-                $response['errors'][] = 'Something went wrong.';
-
-            }
-            return $response;
-        }
-
-    }
-
-    //----------------------------------------------------------
-
     public function getItem(Request $request, $id)
     {
         try{
