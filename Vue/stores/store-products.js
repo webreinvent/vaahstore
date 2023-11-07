911f8e752f241267c2da9049ac9d97c5fb178304
Suraj Kumar <suraj-k001@webreinvent.com>
2023-11-07 12:22:19 +0530

    updated : status section when we enable active and deactive button

--- Vue/stores/store-products.js
+++ Vue/stores/store-products.js
@@ -341,8 +341,25 @@ export const useProductStore = defineStore({
 
         //---------------------------------------------------------------------
 
-        addVendor() {
-            if (this.selected_vendor != null) {
+        selectStatus()
+        {
+            if(this.item.is_active == '1')
+            {
+                let active_status = this.product_status.find((item) => item.name === "Approved");
+                this.item.taxonomy_id_product_status = active_status.id;
+                this.item.status = active_status;
+            }
+            else {
+                let rejected_status = this.product_status.find((item) => item.name === "Rejected");
+                this.item.taxonomy_id_product_status = rejected_status.id;
+                this.item.status = rejected_status;
+            }
+        },
+
+        //---------------------------------------------------------------------
+
+        addVendor(){
+            if (this.selected_vendor != null){
                 let exist = 0;
                 this.item.vendors.forEach((item) => {
                     if (item['vendor']['id'] == this.selected_vendor['id']) {
