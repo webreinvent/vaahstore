98096e11496e6ea0ec14c7da9402d5d082d72dcb
Suraj Kumar <suraj-k001@webreinvent.com>
2023-11-09 17:43:52 +0530

    updated : orders-crud

--- Models/OrderItem.php
+++ Models/OrderItem.php
@@ -549,7 +549,7 @@ class OrderItem extends Model
 //-------------------------------------------------
     public static function deleteOrder($items_id){
         if($items_id){
-            self::whereIn('vh_st_order_id',$items_id)->forcedelete();
+            self::where('vh_st_order_id',$items_id)->forcedelete();
             $response['success'] = true;
             $response['data'] = true;
         }else{
