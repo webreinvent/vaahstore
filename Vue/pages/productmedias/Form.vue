02ff9e723ab47a88c2b49502ef466c68e1e2e48f
Suraj Kumar <suraj-k001@webreinvent.com>
2023-11-21 19:30:52 +0530

    updated : form vue is_active button for all the cruds

--- Vue/pages/productmedias/Form.vue
+++ Vue/pages/productmedias/Form.vue
@@ -244,7 +244,7 @@ const toggleFormMenu = (event) => {
                 <VhField label="Is Active">
                     <InputSwitch v-bind:false-value="0"
                                  v-bind:true-value="1"
-                                 class="p-inputswitch-sm"
+                                 class="p-inputswitch"
                                  name="productmedias-active"
                                  data-testid="productmedias-active"
                                  v-model="store.item.is_active"/>
