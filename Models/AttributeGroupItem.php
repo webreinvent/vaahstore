989e3b6f7ac7aa841fa7204a9486728e7396d4f4
Suraj Kumar <suraj-k001@webreinvent.com>
2023-11-24 11:08:26 +0530

    updated : name and slug validation in all of the cruds

--- Models/AttributeGroupItem.php
+++ Models/AttributeGroupItem.php
@@ -511,8 +511,8 @@ class AttributeGroupItem extends Model
     {
 
         $rules = array(
-            'name' => 'required|max:150',
-            'slug' => 'required|max:150',
+            'name' => 'required|max:250',
+            'slug' => 'required|max:250',
         );
 
         $validator = \Validator::make($inputs, $rules);
--- Models/AttributeValue.php
+++ Models/AttributeValue.php
@@ -519,8 +519,8 @@ class AttributeValue extends Model
     {
 
         $rules = array(
-            'name' => 'required|max:150',
-            'slug' => 'required|max:150',
+            'name' => 'required|max:250',
+            'slug' => 'required|max:250',
         );
 
         $validator = \Validator::make($inputs, $rules);
