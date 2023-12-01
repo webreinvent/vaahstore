b8da42440bd50e4cab113848a2200f79bdef30e2
Suraj Kumar <suraj-k001@webreinvent.com>
2023-12-01 14:08:03 +0530

    updated : search for stores name or slug

--- Models/Store.php
+++ Models/Store.php
@@ -609,12 +609,20 @@ class Store extends Model
         {
             return $query;
         }
-        $search = $filter['q'];
-        $query->where(function ($q) use ($search) {
-            $q->where('name', 'LIKE', '%' . $search . '%')
-                ->orWhere('slug', 'LIKE', '%' . $search . '%')
-                ->orWhere('id', 'LIKE', '%' . $search . '%');
-        });
+        $keywords = explode(' ',$filter['q']);
+        foreach($keywords as $search)
+        {
+            $query->where(function ($q) use ($search) {
+                $q->where(function ($q) use ($search) {
+                    $q->where('name', 'LIKE', '%' . $search . '%')
+                        ->orWhere('slug', 'LIKE', '%' . $search . '%');
+                })
+
+                    ->orWhere('id', 'LIKE', '%' . $search . '%');
+
+            });
+        }
+        
 
     }
     //-------------------------------------------------
