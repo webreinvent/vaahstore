6f5dbd2e9e2e0b7c92e26cd54fe068621ac2e0ae
Sumit Kumar <sumit-k001@webreinvent.com>
2023-12-08 17:50:59 +0530

    Updated: dialog for selected list action
    add a dialog for selected item using checkbox when user perform delete ,restore, trash

--- Vue/vaahvue/pinia/vaah.js
+++ Vue/vaahvue/pinia/vaah.js
@@ -253,39 +253,15 @@ export const vaah = defineStore({
             this.confirmDialog('Delete Confirmation', 'Do you want to Delete record(s)?', callbackOnAccept);
         },
         //----------------------------------------------------------
-
-        confirmDialogDeleteAll(callbackOnAccept)
-        {
-            this.confirmDialog('Delete Confirmation', 'Do you want to Delete all record(s)?', callbackOnAccept);
-        },
-        //----------------------------------------------------------
-
-        confirmDialogActivate(callbackOnAccept)
-        {
-            this.confirmDialog('Activate Confirmation', 'Do you want to Activate all record(s)?', callbackOnAccept);
-        },
-
-        //----------------------------------------------------------
-
-        confirmDialogDeactivate(callbackOnAccept)
-        {
-            this.confirmDialog('Deactivate Confirmation', 'Do you want to Deactivate all record(s)?', callbackOnAccept);
-        },
-
-        //----------------------------------------------------------
-
         confirmDialogTrash(callbackOnAccept)
         {
-            this.confirmDialog('Trash Confirmation', 'Do you want to Trash all record(s)?', callbackOnAccept);
+            this.confirmDialog('Trash Confirmation', 'Do you want to trash record(s)?', callbackOnAccept);
         },
-
         //----------------------------------------------------------
-
         confirmDialogRestore(callbackOnAccept)
         {
-            this.confirmDialog('Restore Confirmation', 'Do you want to Restore all record(s)?', callbackOnAccept);
+            this.confirmDialog('Restore Confirmation', 'Do you want to restore record(s)?', callbackOnAccept);
         },
-
         //----------------------------------------------------------
         clone: function (source)
         {
