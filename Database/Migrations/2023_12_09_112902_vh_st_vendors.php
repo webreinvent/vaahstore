155cca32fbb4704afb9ca0863f23038362415793
Suraj Kumar <suraj-k001@webreinvent.com>
2023-12-09 11:37:43 +0530

    Added : migration file for columns like phone number, enail and address

---
+++ Database/Migrations/2023_12_09_112902_vh_st_vendors.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class VhStVendors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('vh_st_vendors', function (Blueprint $table) {
+            $table->bigInteger('phone_number')->nullable()->after('slug');
+            $table->string('email')->nullable()->after('phone_number');
+            $table->text('address')->nullable()->after('email');
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::table('vh_st_vendors', function (Blueprint $table) {
+            $table->dropColumn('phone_number');
+            $table->dropColumn('email');
+            $table->dropColumn('address');
+        });
+    }
+}
