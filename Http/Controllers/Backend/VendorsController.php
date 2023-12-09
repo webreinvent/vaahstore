1323d75a36ecf5b059b5e1f583a45d25c42851b2
Suraj Kumar <suraj-k001@webreinvent.com>
2023-12-09 13:36:35 +0530

    Added : file uploader for form.vue

--- Http/Controllers/Backend/VendorsController.php
+++ Http/Controllers/Backend/VendorsController.php
@@ -65,6 +65,7 @@ class VendorsController extends Controller
             $data['taxonomy']['product_vendor_status'] = Taxonomy::getTaxonomyByType('product-vendor-status');
             $data['taxonomy']['vendor_status'] = Taxonomy::getTaxonomyByType('vendor-status');
             $data['taxonomy']['business_type'] = Taxonomy::getTaxonomyByType('business-types');
+            $data['urls']['upload'] = route('vh.backend.media.upload');
             $response['success'] = true;
             $response['data'] = $data;
 
