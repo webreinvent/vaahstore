7937e2b5b1e5c1927c3dfbc551797544afbb2ce9
Suraj Kumar <suraj-k001@webreinvent.com>
2023-12-11 17:18:44 +0530

    Updated : migration class name

--- Database/Migrations/2023_12_08_140609_vh_st_vendors.php
+++ Database/Migrations/2023_12_08_140609_vh_st_vendors.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class VhStVendors extends Migration
+class VhStVendorsAddYearsAndServicesColumn extends Migration
 {
     /**
      * Run the migrations.
