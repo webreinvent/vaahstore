8e0385b9274b081c065db18a3dea3ff10ea83457
Suraj Kumar <suraj-k001@webreinvent.com>
2023-12-11 17:31:59 +0530

    updated : class names of migration file

--- Database/Migrations/2023_11_15_115658_add_column_in_vh_st_orders.php
+++ Database/Migrations/2023_11_15_115658_add_column_in_vh_st_orders.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class VhStOrdersAmountColumn extends Migration
+class AddColumnInVhStOrders extends Migration
 {
     /**
      * Run the migrations.
