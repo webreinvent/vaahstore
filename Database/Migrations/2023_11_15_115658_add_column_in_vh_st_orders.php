b8873a2d761834b4f6cd22ec232c307222c9dcf8
Sumit Kumar <sumit-k001@webreinvent.com>
2023-12-11 16:43:46 +0530

    Updated: migration orders

--- Database/Migrations/2023_11_15_115658_add_column_in_vh_st_orders.php
+++ Database/Migrations/2023_11_15_115658_add_column_in_vh_st_orders.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnInVhStOrders extends Migration
+class AddColumnInVhStores extends Migration
 {
     /**
      * Run the migrations.
