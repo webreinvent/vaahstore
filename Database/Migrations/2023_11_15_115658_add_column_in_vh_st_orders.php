e34f1a1ce4547aa1e453d684bccb9ecad61207aa
Sumit Kumar <sumit-k001@webreinvent.com>
2023-12-11 16:43:46 +0530

    Updated: migration orders

---
+++ Database/Migrations/2023_11_15_115658_add_column_in_vh_st_orders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnInVhStores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('vh_st_orders', function (Blueprint $table) {
+            $table->double('amount')->change();
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::table('vh_st_orders', function (Blueprint $table) {
+            $table->integer('amount')->change();
+        });
+    }
+}
