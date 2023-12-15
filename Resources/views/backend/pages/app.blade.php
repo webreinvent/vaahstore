6aa43130f98b273a58ab66c774f73e4260e21776
Sumit Kumar <sumit-k001@webreinvent.com>
2023-12-15 16:41:25 +0530

    Updated: index css CDN

--- Resources/views/backend/pages/app.blade.php
+++ Resources/views/backend/pages/app.blade.php
@@ -1,7 +1,10 @@
 @extends(((version_compare(config('vaahcms.version'), '2.0.0', '<' )) ? 'vaahcms::backend.vaahone.layouts.backend' : 'vaahcms::backend.vaahtwo.layouts.backend' ))
 
 @section('vaahcms_extend_backend_css')
-
+    @if(env('MODULE_STORE_ENV') !== 'develop')
+        <link href="{{vh_module_assets_url("Store", "build/index.css")}}"
+              rel="stylesheet" media="screen"></link>
+    @endif
 @endsection
 
 
