e65b4226128b5b463c41cedf5f3facdede00e501
Himanshu Mehta <himanshu-m001@webreinvent.com>
2023-12-19 13:18:04 +0530

    Added: date search in filter

--- Models/Brand.php
+++ Models/Brand.php
@@ -163,6 +163,33 @@ class Brand extends Model
 
     //-------------------------------------------------
 
+    public function scopeDateFilter($query, $filter)
+    {
+
+
+        if(!isset($filter['date'])
+            || is_null($filter['date'])
+        )
+        {
+            return $query;
+        }
+
+        $dates = $filter['date'];
+        $from = \Carbon::parse($dates[0])
+            ->startOfDay()
+            ->toDateTimeString();
+
+        $to = \Carbon::parse($dates[1])
+            ->endOfDay()
+            ->toDateTimeString();
+
+        return $query->whereBetween('created_at', [$from, $to]);
+
+    }
+
+
+    //-------------------------------------------------
+
     public function scopeActive($query)
     {
         return $query->where('is_active', 1);
@@ -339,6 +366,7 @@ class Brand extends Model
         $list->trashedFilter($request->filter);
         $list->searchFilter($request->filter);
         $list->statusFilter($request->filter);
+        $list->dateFilter($request->filter);
 
         $rows = config('vaahcms.per_page');
 
