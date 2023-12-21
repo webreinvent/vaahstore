8f8df8a710be1cf482b0cf28dfdf8768981dd810
Sumit Kumar <sumit-k001@webreinvent.com>
2023-12-21 16:49:31 +0530

    Updated: reset filter message

--- Vue/stores/store-productvariations.js
+++ Vue/stores/store-productvariations.js
@@ -705,17 +705,13 @@ export const useProductVariationStore = defineStore({
         {
             //reset query strings
             await this.resetQueryString();
-
+            vaah().toastSuccess(['Action Was Successful']);
             //reload page list
             await this.getList();
         },
         //---------------------------------------------------------------------
         async resetQueryString()
         {
-            if(this.query.filter.q)
-            {
-                vaah().toastSuccess(['Action Was Successful']);
-            }
 
             for(let key in this.query.filter)
             {
