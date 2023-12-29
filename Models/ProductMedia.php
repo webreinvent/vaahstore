9fd13c2d6bca614c6f518269b9484b84a8f3ea95
Sumit Kumar <sumit-k001@webreinvent.com>
2023-12-29 12:06:59 +0530

    Updated: delete record

--- Models/ProductMedia.php
+++ Models/ProductMedia.php
@@ -994,7 +994,7 @@ class ProductMedia extends Model
     //-------------------------------------------------
     public static function deleteProductVariation($items_id){
 
-        if ($items_id) {
+        if (is_array($items_id)) {
             $items_exist = self::whereIn('vh_st_product_variation_id', $items_id);
 
             if ($items_exist) {
@@ -1005,7 +1005,14 @@ class ProductMedia extends Model
                 $response['success'] = true;
                 $response['data'] = false;
             }
-        } else {
+        } elseif ($items_id)
+        {
+                self::where('vh_st_product_variation_id', $items_id)->forceDelete();
+                $response['success'] = true;
+                $response['data'] = true;
+        }
+
+        else {
             // If $items_id is not set, return an error
             $response['error'] = true;
             $response['data'] = false;
