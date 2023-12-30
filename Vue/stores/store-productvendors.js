298e755d81a8a75070f188df97d2b2d28b4f085c
Shivam Gupta <Shivam-g001@webreinvent.com>
2023-12-30 17:02:57 +0530

    Removed : unnecessary code for count item

--- Vue/stores/store-productvendors.js
+++ Vue/stores/store-productvendors.js
@@ -83,8 +83,7 @@ export const useProductVendorStore = defineStore({
         status: null,
         product_variation_suggestion:null,
         disable_approved_by:true,
-        prev_list:[],
-        current_list:[]
+
     }),
     getters: {
 
@@ -377,6 +376,9 @@ export const useProductVendorStore = defineStore({
             if(data)
             {
                 this.list = data;
+                if (this.action.type === 'trash' || this.action.type === 'restore'){
+                    this.action.items = data.data;
+                }
             }
         },
         //---------------------------------------------------------------------
@@ -513,7 +515,7 @@ export const useProductVendorStore = defineStore({
             {
                 item = this.item;
             }
-
+            this.action.type = type;
             this.form.action = type;
 
             let ajax_url = this.ajax_url;
@@ -589,31 +591,15 @@ export const useProductVendorStore = defineStore({
             if(data)
             {
                 this.item = data;
-                this.prev_list =this.list.data;
                 this.item.added_by = data.added_by;
                 this.item.taxonomy_id_product_vendor_status = data.status;
-
                 this.item.vh_st_product_variation_id = data.product_variation;
                 await this.getList();
                 await this.formActionAfter(data);
                 this.getItemMenu();
                 this.getFormMenu();
             }
-            this.current_list=this.list.data
-            this.compareList(this.prev_list,this.current_list)
 
-        },
-        //---------------------------------------------------------------------
-        compareList(prev_list, current_list) {
-
-            const removed_Items = prev_list.filter(previous_item => !current_list.some(current_item => current_item.id === previous_item.id));
-
-            const removed_item_present_in_current_list = removed_Items.some(removed_item =>
-                current_list.some(current_item => current_item.id === removed_item.id)
-            );
-            if (!removed_item_present_in_current_list) {
-                this.action.items = this.action.items.filter(item => !removed_Items.some(removed_item => removed_item.id === item.id));
-            }
         },
         //---------------------------------------------------------------------
         async formActionAfter (data)
