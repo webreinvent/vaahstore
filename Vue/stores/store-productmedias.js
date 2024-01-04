ee87c47a4fe2e47cb862702c42e692845304bfe4
Shivam Gupta <Shivam-g001@webreinvent.com>
2024-01-04 12:02:17 +0530

    Updated: confirmation box on bulk actions

--- Vue/stores/store-productmedias.js
+++ Vue/stores/store-productmedias.js
@@ -691,6 +691,30 @@ export const useProductMediaStore = defineStore({
             vaah().confirmDialogDelete(this.listAction);
         },
         //---------------------------------------------------------------------
+        confirmRestoreAll()
+        {
+            this.action.type = 'restore-all';
+            vaah().confirmDialogRestore(this.listAction);
+        },
+        //---------------------------------------------------------------------
+        confirmTrashAll()
+        {
+            this.action.type='trash-all';
+            vaah().confirmDialogTrash(this.listAction);
+        },
+        //---------------------------------------------------------------------
+        confirmActivateAll()
+        {
+            this.action.type='activate-all';
+            vaah().confirmDialogActivate(this.listAction);
+        },
+        //---------------------------------------------------------------------
+        confirmDeactivateAll()
+        {
+            this.action.type='deactivate-all';
+            vaah().confirmDialogDeactivate(this.listAction);
+        },
+        //---------------------------------------------------------------------
         async delayedSearch()
         {
             let self = this;
@@ -885,13 +909,13 @@ export const useProductMediaStore = defineStore({
                 {
                     label: 'Mark all as active',
                     command: async () => {
-                        await this.listAction('activate-all')
+                        this.confirmActivateAll();
                     }
                 },
                 {
                     label: 'Mark all as inactive',
                     command: async () => {
-                        await this.listAction('deactivate-all')
+                        this.confirmDeactivateAll();
                     }
                 },
                 {
@@ -901,14 +925,14 @@ export const useProductMediaStore = defineStore({
                     label: 'Trash All',
                     icon: 'pi pi-times',
                     command: async () => {
-                        await this.listAction('trash-all')
+                        this.confirmTrashAll();
                     }
                 },
                 {
                     label: 'Restore All',
                     icon: 'pi pi-replay',
                     command: async () => {
-                        await this.listAction('restore-all')
+                        this.confirmRestoreAll();
                     }
                 },
                 {
