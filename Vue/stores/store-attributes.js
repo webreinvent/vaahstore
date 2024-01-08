42ea5170b4f2eb26a6c55b225e76f0f2db474a04
Shivam Gupta <Shivam-g001@webreinvent.com>
2024-01-08 15:41:40 +0530

    Updated: item count on trash and restore

--- Vue/stores/store-attributes.js
+++ Vue/stores/store-attributes.js
@@ -71,7 +71,9 @@ export const useAttributeStore = defineStore({
         item_menu_list: [],
         item_menu_state: null,
         form_menu_list: [],
-        selected_dates:[]
+        selected_dates:[],
+        prev_list:[],
+        current_list:[]
     }),
     getters: {
 
@@ -493,10 +495,25 @@ export const useAttributeStore = defineStore({
         {
             if(data)
             {
+                this.prev_list =this.list.data;
                 await this.getList();
                 await this.formActionAfter(data);
                 this.getItemMenu();
                 this.getFormMenu();
+            }
+            this.current_list=this.list.data
+            this.compareList(this.prev_list,this.current_list)
+        },
+        //---------------------------------------------------------------------
+        compareList(prev_list, current_list) {
+
+            const removed_Items = prev_list.filter(previous_item => !current_list.some(current_item => current_item.id === previous_item.id));
+
+            const removed_item_present_in_current_list = removed_Items.some(removed_item =>
+                current_list.some(current_item => current_item.id === removed_item.id)
+            );
+            if (!removed_item_present_in_current_list) {
+                this.action.items = this.action.items.filter(item => !removed_Items.some(removed_item => removed_item.id === item.id));
             }
         },
         //---------------------------------------------------------------------
@@ -725,6 +742,7 @@ export const useAttributeStore = defineStore({
         {
             this.item = vaah().clone(this.assets.empty_item);
             this.$router.push({name: 'attributes.index'})
+            this.attribute_new_value=null;
         },
         //---------------------------------------------------------------------
         toForm()
