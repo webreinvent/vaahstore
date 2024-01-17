fc78f7b6baeb4662a6111b38d93c327ab2bb44b4
Shivam Gupta <Shivam-g001@webreinvent.com>
2024-01-17 13:07:08 +0530

    Updated: build

--- Vue/pages/attributegroups/components/Table.vue
+++ Vue/pages/attributegroups/components/Table.vue
@@ -52,16 +52,16 @@ const useVaah = vaah();
              </Column>
 
 
-                <Column field="updated_at" header="Updated"
-                        v-if="store.isViewLarge()"
-                        style="width:150px;"
-                        :sortable="true">
+<!--                <Column field="updated_at" header="Updated"-->
+<!--                        v-if="store.isViewLarge()"-->
+<!--                        style="width:150px;"-->
+<!--                        :sortable="true">-->
 
-                    <template #body="prop">
-                        {{useVaah.ago(prop.data.updated_at)}}
-                    </template>
+<!--                    <template #body="prop">-->
+<!--                        {{useVaah.ago(prop.data.updated_at)}}-->
+<!--                    </template>-->
 
-                </Column>
+<!--                </Column>-->
 
             <Column field="is_active" v-if="store.isViewLarge()"
 
