7ecfd7b1728d1fe230dd8609a7183047a645ecc3
Suraj Kumar <suraj-k001@webreinvent.com>
2024-01-27 08:04:24 +0530

    updated : search query to search on the basis of id

--- Models/ProductStock.php
+++ Models/ProductStock.php
@@ -316,20 +316,13 @@ class ProductStock extends VaahModel
         {
             return $query;
         }
-        $keywords = explode(' ',$filter['q']);
-        foreach($keywords as $search)
-        {
-            $query->where(function ($q) use ($search) {
-                $q->where(function ($q) use ($search) {
-                    $q->where('name', 'LIKE', '%' . $search . '%')
-                        ->orWhere('slug', 'LIKE', '%' . $search . '%');
-                })
+        $search = $filter['q'];
 
-                    ->orWhere('id', 'LIKE', '%' . $search . '%');
+        $query->where(function ($q) use ($search) {
+            $q->Where('id', 'LIKE', '%' . $search . '%');
 
-            });
-        }
-
+        });
+        
     }
 
     //-------------------------------------------------
