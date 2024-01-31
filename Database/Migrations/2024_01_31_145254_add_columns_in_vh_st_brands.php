03d543281dc1d0ac3ae1689ecdb734d08218c46a
Sumit Kumar <sumit-k001@webreinvent.com>
2024-01-31 15:03:37 +0530

    Added: migration

---
+++ Database/Migrations/2024_01_31_145254_add_columns_in_vh_st_brands.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsInVhStBrands extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('vh_st_brands', function($table) {
+            $table->string('image')->nullable()->index()->after('slug');
+            $table->string('meta_title')->nullable()->index()->after('image');
+            $table->text('meta_description')->nullable()->after('meta_title');
+            $table->text('meta_keywords')->nullable()->index()->after('meta_description');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vh_st_product_variations', function($table) {
+            $table->dropColumn(['image','meta_title','meta_description','meta_keywords']);
+        });
+    }
+}
