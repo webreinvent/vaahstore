026ddf69f14bfa52f06b71b6fc4b9fdd51a0eeb8
Suraj Kumar <suraj-k001@webreinvent.com>
2024-02-06 11:51:32 +0530

    Removed : extra css

--- Vue/pages/wishlists/Form.vue
+++ Vue/pages/wishlists/Form.vue
@@ -117,7 +117,6 @@ const toggleFormMenu = (event) => {
                         name="wishlists-user"
                         :dropdown="true"
                         optionLabel="first_name"
-                        style="max-width:100%;word-wrap:break-word;">
                         forceSelection
                         :pt="{
                           token: {
@@ -132,7 +131,7 @@ const toggleFormMenu = (event) => {
                                 }  },
                           panel: { class: 'w-16rem ' }
                             }"
-
+                        >
                     </AutoComplete>
                 </VhField>
 
