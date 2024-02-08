bf46c91e97d973a8d01e16f316a6594bc3351333
Sumit Kumar <sumit-k001@webreinvent.com>
2024-02-08 15:09:47 +0530

    Updated: status

--- Http/Controllers/Backend/VendorsController.php
+++ Http/Controllers/Backend/VendorsController.php
@@ -68,7 +68,7 @@ class VendorsController extends Controller
             $data['taxonomy']['product_vendor_status'] = Taxonomy::getTaxonomyByType('product-vendor-status');
             $data['taxonomy']['vendor_status'] = Taxonomy::getTaxonomyByType('vendor-status');
             $data['taxonomy']['business_type'] = Taxonomy::getTaxonomyByType('business-type');
-            $data['taxonomy']['product_status'] = Taxonomy::getTaxonomyByType('product-status');
+            $data['taxonomy']['product_status'] = Taxonomy::getTaxonomyByType('store-status');
             $data['urls']['upload'] = route('vh.backend.media.upload');
             $response['success'] = true;
             $response['data'] = $data;
