c2786fa7578621b050deadeb3179f58588d92724
Sumit Kumar <sumit-k001@webreinvent.com>
2024-02-29 06:15:07 +0530

    Added: Min Max Variation Price Range

--- Vue/stores/store-productvendors.js
+++ Vue/stores/store-productvendors.js
@@ -1388,6 +1388,30 @@ export const useProductVendorStore = defineStore({
             }
         },
         //---------------------------------------------------------------------
+
+
+         calculatePriceRange (variations) {
+            if (!variations || variations.length === 0) {
+                return '';
+            }
+            console.log(variations)
+
+            let minPrice = variations[0].price;
+            let maxPrice = variations[0].price;
+
+            for (let i = 1; i < variations.length; i++) {
+                const price = variations[i].price;
+                if (price < minPrice) {
+                    minPrice = price;
+                }
+                if (price > maxPrice) {
+                    maxPrice = price;
+                }
+            }
+
+            return `${minPrice} - ${maxPrice}`;
+        }
+
         //---------------------------------------------------------------------
 
     }
