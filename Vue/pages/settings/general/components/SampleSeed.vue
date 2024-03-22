de040b0c940de3185e1aa83d5b390549c9252c70
Sumit Kumar <sumit-k001@webreinvent.com>
2024-03-22 15:03:08 +0530

    Updated: sample seed component

--- Vue/pages/settings/general/components/SampleSeed.vue
+++ Vue/pages/settings/general/components/SampleSeed.vue
@@ -4,9 +4,17 @@
 
     </div>
     <div class="card flex justify-content-center">
-        <Dropdown v-model="selectedCrud" :options="crud" optionLabel="name" placeholder="Select a Crud" class="w-full md:w-14rem mr-1"  />
-        <InputText id="username" v-model="quantity_val" placeholder="Enter Quantity" class="mr-1" />
-        <Button label="Submit" @click="store.createBulkRecords([selectedCrud.name ,quantity_val])" />
+        <Dropdown class="w-full md:w-14rem mr-1"
+                  v-model="store.selected_crud"
+                  :options="store.crud_options"
+                  optionLabel="label"
+                  optionValue="value"
+                  id="account-status"
+                  name="users-status"
+                  data-testid="users-status"
+        />
+        <InputText id="input_text" v-model="store.quantity" placeholder="Enter Quantity" class="mr-1" />
+        <Button label="Submit" @click="store.createBulkRecords()" />
     </div>
 </template>
 
@@ -14,24 +22,5 @@
 import {useSettingStore} from '../../../../stores/store-settings'
 
 const store = useSettingStore();
-
-import { ref } from "vue";
-
-const value = ref("");
-const items = ref([]);
-const quantity_val = ref(null);
-
-const selectedCrud = ref();
-const crud = ref([
-    { name: 'Store' },
-    { name: 'Rome' },
-    { name: 'London' },
-    { name: 'Istanbul' },
-    { name: 'Paris' }
-]);
-
-const search = (event) => {
-    items.value = [...Array(10).keys()].map((item) => event.query + '-' + item);
-}
 
 </script>
