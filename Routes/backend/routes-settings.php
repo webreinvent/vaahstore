60259c2df8c3dd61dde112986ae27156b165687a
Sumit Kumar <sumit-k001@webreinvent.com>
2024-03-22 11:09:59 +0530

    Added: Component and route file

---
+++ Routes/backend/routes-settings.php
@@ -0,0 +1,30 @@
+<?php
+
+Route::group(
+    [
+        'prefix' => 'backend/store/settings',
+
+        'middleware' => ['web', 'has.backend.access'],
+
+        'namespace' => 'Backend',
+],
+function () {
+    /**
+     * Get Assets
+     */
+    Route::get('/assets', 'SettingsController@getAssets')
+        ->name('vh.backend.store.settings.assets');
+    /**
+     * Get List
+     */
+    Route::get('/', 'SettingsController@getList')
+        ->name('vh.backend.store.settings.list');
+    /**
+     * Update List
+     */
+    Route::match(['put', 'patch'], '/', 'SettingsController@updateList')
+        ->name('vh.backend.store.settings.list.update');
+
+
+
+});
---
+++ Vue/pages/settings/general/components/SampleSeed.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div>
+
+    </div>
+    <div class="card flex justify-content-center">
+        {{quantity_val}} {{selectedCrud}}
+        <Dropdown v-model="selectedCrud" :options="crud" optionLabel="name" placeholder="Select a Crud" class="w-full md:w-14rem" />
+        <InputText id="username" v-model="quantity_val" placeholder="Enter Quantity" />
+        <Button label="Submit" @click="store.createBulkRecords([selectedCrud.name ,quantity_val])" />
+    </div>
+</template>
+
+<script setup>
+import {useSettingStore} from '../../../../stores/store-settings'
+
+const store = useSettingStore();
+
+import { ref } from "vue";
+
+const value = ref("");
+const items = ref([]);
+const quantity_val = ref(null);
+
+const selectedCrud = ref();
+const crud = ref([
+    { name: 'Store' },
+    { name: 'Rome' },
+    { name: 'London' },
+    { name: 'Istanbul' },
+    { name: 'Paris' }
+]);
+
+const search = (event) => {
+    items.value = [...Array(10).keys()].map((item) => event.query + '-' + item);
+}
+
+</script>
