182d796720d4d18dc23873e02473b4888be3b409
Sumit Kumar <sumit-k001@webreinvent.com>
2024-04-03 09:27:04 +0530

    Updated: data-testid

--- Vue/pages/products/components/Filters.vue
+++ Vue/pages/products/components/Filters.vue
@@ -196,7 +196,7 @@ const store = useProductStore();
 
                         <InputNumber
                             v-model="store.query.filter.min_quantity"
-                            data-testid="product-variation-filter-min_quantity"
+                            data-testid="product-filter-min_quantity"
                             placeholder="Enter minimum quantity"
                             @input="store.minQuantity($event)"
                             class="w-14rem mt-2"
@@ -204,7 +204,7 @@ const store = useProductStore();
                         />
                         <InputNumber
                             v-model="store.query.filter.max_quantity"
-                            data-testid="product-variation-filter-max_quantity"
+                            data-testid="product-filter-max_quantity"
                             placeholder="Enter maximum quantity"
                             @input="store.maxQuantity($event)"
                             class="w-14rem mt-2"
