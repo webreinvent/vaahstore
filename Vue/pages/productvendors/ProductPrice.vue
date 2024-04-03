5d8502d758d42aaa345d77058b52a44020c81029
Shivam Gupta <shivam-g001@webreinvent.com>
2024-04-03 12:15:12 +0530

    Updated: null on mount

--- Vue/pages/productvendors/ProductPrice.vue
+++ Vue/pages/productvendors/ProductPrice.vue
@@ -11,7 +11,8 @@ const store = useProductVendorStore();
 const route = useRoute();
 
 onMounted(async () => {
-    if (route.params?.id && store.product_variation_list.length <= 0) {
+    if (route.params?.id ) {
+        console.log(store.product_variation_list)
         await store.getItem(route.params.id);
         await store.searchVariationOfProduct();
     }
