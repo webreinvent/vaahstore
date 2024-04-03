f0395b769572015b0b5d9a037aa60776bc65f90a
Sumit Kumar <sumit-k001@webreinvent.com>
2024-04-03 05:42:23 +0530

    Updated: mail template

--- Models/ProductVariation.php
+++ Models/ProductVariation.php
@@ -1263,12 +1263,22 @@ class ProductVariation extends VaahModel
                     $message .= '<tr>';
                     $message .= '<td>' . $product_name . '</td>';
                     $message .= '<td>' . $variation_name . '</td>';
-                    $message .= '<td><a href="'.url("/").'/backend/store#/productvariations?page=1&rows=20&filter[in_stock][]='.true.'">View</a></td>';
                     $message .= '</tr>';
                 }
 
 
             $message .= '</table>';
+            $message .= '<p style="margin-top: 0.6rem; font-size: 15px">For more low stock variations, click here</p>';
+            $message .= '<p><a href="'.url("/").'/backend/store#/productvariations?page=1&rows=20&filter[in_stock][]='.true.'"
+                         style="background-color: #4CAF50; color: white; padding: 8px 15px; border: none; border-radius: 25px;
+                         text-decoration: none; display: inline-block; cursor: pointer; font-size: 14px; width: 7rem;
+                         text-align: center; margin-top: 0.3rem;">View</a></p>';
+
+
+
+
+
+
             if ($filtered_data->isNotEmpty()) {
                 $send_mail = UserBase::notifySuperAdmins($subject, $message);
             }
