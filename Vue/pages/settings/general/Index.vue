2af67676cd67a5d9e6f381cfad26ea68d5003662
Sumit Kumar <sumit-k001@webreinvent.com>
2024-04-03 11:33:23 +0530

    Updated: heading for bulk records

--- Vue/pages/settings/general/Index.vue
+++ Vue/pages/settings/general/Index.vue
@@ -25,7 +25,7 @@ onMounted(async () => {
         <Card>
             <template #header>
                 <div class="flex justify-content-between align-items-center w-full">
-                    <h5 class="font-semibold text-lg">Create Bulk Records For Vaah Store Cruds</h5>
+                    <h5 class="font-semibold text-lg">Create Bulk Records For VaahStore Cruds</h5>
                 </div>
             </template>
 
