adc5659548ad759dd87e2ac5b717294de9dbcacd
Shivam Gupta <shivam-g001@webreinvent.com>
2024-04-10 12:28:16 +0530

    Added: migration for product categories

---
+++ Database/Migrations/2024_04_10_122603_vh_st_product_categories.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class VhStProductCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('vh_st_product_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid')->nullable()->index();
+            $table->integer('vh_st_product_id')->nullable()->index();
+            $table->integer('category_id')->nullable()->index();
+
+
+            //----common fields
+            $table->text('meta')->nullable();
+            $table->integer('created_by')->nullable()->index();
+            $table->integer('updated_by')->nullable()->index();
+            $table->integer('deleted_by')->nullable()->index();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->index(['created_at', 'updated_at', 'deleted_at']);
+            //----/common fields
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vh_st_product_categories');
+    }
+
+}
