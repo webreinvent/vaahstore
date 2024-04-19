eabb87df77bb0f966ef8b31da1f4c66576952213
Shivam Gupta <shivam-g001@webreinvent.com>
2024-04-19 15:15:02 +0530

    Added: price column

--- Vue/pages/productvariations/components/Table.vue
+++ Vue/pages/productvariations/components/Table.vue
@@ -81,7 +81,14 @@ const route = useRoute();
                  </template>
 
              </Column>
+            <Column field="price" header="Price"
+                    :sortable="true">
 
+                <template #body="prop">
+                    {{prop.data.price}}
+                </template>
+
+            </Column>
             <Column field="stock_status" header="Stock Status"
                     :sortable="true">
 
@@ -110,16 +117,7 @@ const route = useRoute();
              </Column>
 
 
-                <Column field="updated_at" header="Updated"
-                        v-if="store.isViewLarge()"
-                        style="width:150px;"
-                        :sortable="true">
-
-                    <template #body="prop">
-                        {{useVaah.ago(prop.data.updated_at)}}
-                    </template>
-
-                </Column>
+               
 
             <Column
                 field="is_active"
