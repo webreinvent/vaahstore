defb8b9893a72aeba980ca07991e6d8f4c9fe39d
Sumit Kumar <sumit-k001@webreinvent.com>
2024-04-24 19:48:25 +0530

    Updated: optimize checkbox functionality

--- Vue/pages/settings/general/components/SampleSeed.vue
+++ Vue/pages/settings/general/components/SampleSeed.vue
@@ -17,147 +17,159 @@
     -->
 
 
-    <Button type="button" label="Fill All Inputs" icon="pi pi-arrow-down" @click="store.fillAll()" style="margin-left: 50rem"/>
+<!--    <Button type="button" label="Fill All Inputs" icon="pi pi-arrow-down" @click="store.fillAll()" style="margin-left: 50rem"/>-->
 
-    <Button type="button" label="Reset All Inputs " icon="pi pi-arrow-down" @click="store.resetAll()"/>
+<!--    <Button type="button" label="Reset All Inputs " icon="pi pi-refresh" @click="store.resetAll()"/>-->
 
-    <div class="container">
+<!--    <div class="container">-->
 
-        <div class="card  justify-content-center gap-3 mt-5">
+<!--        <div class="card  justify-content-center gap-3 mt-5">-->
 
-            <div class="flex align-items-center">
-                <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-                <label  class="ml-2"> All </label>
-                <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" style="margin-left: 8.6rem" />
-            </div>
+<!--            <div class="flex align-items-center">-->
+<!--                <Checkbox v-model="store.is_checked" value="All" />-->
+<!--                <label  class="ml-2"> All </label>-->
+<!--                <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" style="margin-left: 8.6rem" />-->
+<!--            </div>-->
 
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Store  </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2"  style="margin-left: 7.5rem"/>
-        </div>
-
-
-        <div class="flex align-items-center ">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Store Payment Methods </label>
-            <InputNumber placeholder="Enter quantity" class="ml-2 mt-2" v-model="store.input_number_value" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Vendor </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6.9rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Vendor Products </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 3.2rem" />
-        </div>
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked" value="Store" />-->
+<!--            <label  class="ml-2"> Store  </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2"  style="margin-left: 7.5rem"/>-->
+<!--        </div>-->
 
 
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Products </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6rem" />
-        </div>
+<!--        <div class="flex align-items-center ">-->
+<!--            <Checkbox v-model="store.is_checked" value="StorePaymentMethods" />-->
+<!--            <label  class="ml-2"> Store Payment Methods </label>-->
+<!--            <InputNumber placeholder="Enter quantity" class="ml-2 mt-2" v-model="store.input_number_value" />-->
+<!--        </div>-->
 
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Product Variations </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 2.4rem" />
-        </div>
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked" value="Vendor" />-->
+<!--            <label  class="ml-2"> Vendor </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6.9rem" />-->
+<!--        </div>-->
 
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Product Attributes </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 2.4rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Product Medias </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 3.4rem" />
-        </div>
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked" value="VendorProducts" />-->
+<!--            <label  class="ml-2"> Vendor Products </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 3.2rem" />-->
+<!--        </div>-->
 
 
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Product Stocks </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 3.4rem" />
-        </div>
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked"  value="Products" />-->
+<!--            <label  class="ml-2"> Products </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked"  value="ProductVariations" />-->
+<!--            <label  class="ml-2"> Product Variations </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 2.4rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked" value="Product Attributes" />-->
+<!--            <label  class="ml-2"> Product Attributes </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 2.4rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked" value="ProductMedias" />-->
+<!--            <label  class="ml-2"> Product Medias </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 3.4rem" />-->
+<!--        </div>-->
+
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="store.is_checked"  value="ProductStocks" />-->
+<!--            <label  class="ml-2"> Product Stocks </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 3.4rem" />-->
+<!--        </div>-->
 
 
 
-    </div>
+<!--    </div>-->
+
+<!--    <div class="card justify-content-center gap-3 mt-5">-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked" value="Brands" />-->
+<!--            <label  class="ml-2"> Brands </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6.6rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked" value="WareHouse" />-->
+<!--            <label  class="ml-2"> WareHouse </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 5rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked" value="Attributes" />-->
+<!--            <label  class="ml-2"> Attributes </label>-->
+<!--            <InputNumber placeholder="Enter quantity"  v-model="store.input_number_value" class=" mt-2" style="margin-left: 5.6rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked"  value="AttributesGroup" />-->
+<!--            <label  class="ml-2"> Attributes Group  </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 3rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked"  value="Orders" />-->
+<!--            <label  class="ml-2"> Orders </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6.6rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked"  value="PaymentsMethods" />-->
+<!--            <label  class="ml-2"> Payments Methods </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 2rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked"  value="Addresses" />-->
+<!--            <label  class="ml-2"> Addresses </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 5.2rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked"  value="Wishlists" />-->
+<!--            <label  class="ml-2"> Wishlists </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 5.8rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked"  value="Customers" />-->
+<!--            <label  class="ml-2"> Customers </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 5rem" />-->
+<!--        </div>-->
+
+<!--        <div class="flex align-items-center">-->
+<!--            <Checkbox v-model="is_checked"  value="CustomersGroups" />-->
+<!--            <label  class="ml-2"> Customers Groups </label>-->
+<!--            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 2.1rem" />-->
+<!--        </div>-->
+<!--    </div>-->
+
+<!--    </div>-->
+
+<!--    <Button label="Submit" @click="store.createBulkRecords()" style="width: 10rem; margin-top: 1.5rem; border-radius: 4rem" />-->
+
+
+
 
     <div class="card justify-content-center gap-3 mt-5">
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Brands </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6.6rem" />
+        <div v-for="item in store.crud_options" class="flex align-items-center mb-3">
+            <Checkbox v-model="item.isChecked" :value="item.isChecked" class="ml-3" @click="store.checkAll(item)" />
+            <label class="ml-3" >{{ item.label }}</label>
+            <InputNumber placeholder="Enter quantity" class="ml-3" v-model="item.quantity" />
         </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> WareHouse </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 5rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Attributes </label>
-            <InputNumber placeholder="Enter quantity"  v-model="store.input_number_value" class=" mt-2" style="margin-left: 5.6rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Attributes Group  </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 3rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Orders </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 6.6rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Payments Methods </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 2rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Addresses </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 5.2rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Wishlists </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class=" mt-2" style="margin-left: 5.8rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Customers </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 5rem" />
-        </div>
-
-        <div class="flex align-items-center">
-            <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-            <label  class="ml-2"> Customers Groups </label>
-            <InputNumber placeholder="Enter quantity" v-model="store.input_number_value" class="mt-2" style="margin-left: 2.1rem" />
-        </div>
+        <Button label="Submit" @click="store.createBulkRecords()" style="width: 10rem; margin-top: 1.5rem; border-radius: 4rem" />
     </div>
-
-    </div>
-    <Button label="Submit" style="width: 10rem; margin-top: 1.5rem; border-radius: 4rem" />
-
 
 </template>
 
@@ -177,7 +189,7 @@ onMounted(async () => {
     store.selected_crud = null;
 });
 
-const pizza = ref()
+const is_checked = ref()
 
 
 
