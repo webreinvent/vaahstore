7c73ef45502795e1dc9c6a0c90472f586184c4f0
Sumit Kumar <sumit-k001@webreinvent.com>
2024-05-10 13:06:53 +0530

    Added: language and language string file
     language and language string file for vaah-store

--- Database/Seeds/DatabaseTableSeeder.php
+++ Database/Seeds/DatabaseTableSeeder.php
@@ -38,11 +38,24 @@ class DatabaseTableSeeder extends Seeder
         $this->seedDefaultBrand();
         $this->seedDefaultPrdouct();
         $this->seedRoles();
+        $this->seedLanguages();
         $this->seedLanguageCategories();
+        $this->seedLanguageStrings();
+
 
         $seeder = new SettingTableSeeder();
         $seeder->run();
 
+    }
+
+    //--------------------------------------------------------------
+
+    public function getListFromJson($json_file_name)
+    {
+        $json_file = __DIR__."/json/".$json_file_name;
+        $jsonString = file_get_contents($json_file);
+        $list = json_decode($jsonString, true);
+        return $list;
     }
 
     //---------------------------------------------------------------
@@ -190,6 +203,69 @@ class DatabaseTableSeeder extends Seeder
                     ->update($item);
             }
         }
+    }
+
+    public function seedLanguages()
+    {
+
+        $list = $this->getListFromJson("languages.json");
+
+        foreach($list as $item)
+        {
+            $exist = DB::table( 'vh_lang_languages' )
+                ->where( 'locale_code_iso_639', $item['locale_code_iso_639'] )
+                ->first();
+
+            if (!$exist){
+
+                if($item['locale_code_iso_639'] == 'en')
+                {
+                    $item['default'] = 1;
+                }
+
+                DB::table( 'vh_lang_languages' )->insert( $item );
+            }
+        }
+
+    }
+
+    public function seedLanguageStrings()
+    {
+
+        $list = $this->getListFromJson("language_strings.json");
+
+        foreach($list as $item)
+        {
+
+            $item['slug'] = Str::slug($item['name'],'_');
+
+            $lang = DB::table( 'vh_lang_languages' )
+                ->where( 'locale_code_iso_639', $item['locale_code_iso_639'] )
+                ->first();
+
+            $exist = DB::table( 'vh_lang_strings' )
+                ->where('vh_lang_language_id', $lang->id)
+                ->where( 'slug',  $item['slug'] )
+                ->first();
+
+            $cat = DB::table( 'vh_lang_categories' )
+                ->where( 'slug', $item['category'] )
+                ->first();
+
+
+            if (!$exist && $lang && $cat){
+
+                $item['vh_lang_language_id'] = $lang->id;
+
+                $item['vh_lang_category_id'] = $cat->id;
+
+                unset($item['category']);
+                unset($item['locale_code_iso_639']);
+
+                DB::table( 'vh_lang_strings' )->insert( $item );
+            }
+        }
+
     }
 
 }
