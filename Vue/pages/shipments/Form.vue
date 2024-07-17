5d4552630048c2df4908fc8eeb2064b422a59df6
Shivam Gupta <shivam-g001@webreinvent.com>
2024-07-17 11:49:00 +0530

    Update: create form structure

--- Vue/pages/shipments/Form.vue
+++ Vue/pages/shipments/Form.vue
@@ -4,13 +4,10 @@ import { useShipmentStore } from '../../stores/store-shipments'
 
 import VhField from './../../vaahvue/vue-three/primeflex/VhField.vue'
 import {useRoute} from 'vue-router';
-import {useRootStore} from '../../stores/root'
-import {useShipmentStore} from "../../stores/store-shipments";
-import {vaah} from "../../vaahvue/pinia/vaah";
+
+
 const store = useShipmentStore();
 const route = useRoute();
-const root = useRootStore();
-const useVaah = vaah();
 
 onMounted(async () => {
     /**
@@ -22,51 +19,14 @@ onMounted(async () => {
         await store.getItem(route.params.id);
     }
 
-
-
-
-
-
-// onMounted(async () => {
-//     /**
-//      * Fetch the record from the database
-//      */
-//
-//     if(store.assets_is_fetching === true)
-//     {
-//         await store.getAssets();
-//     }
-//     await store.watchRoutes(route);
-//
-//     if ((!store.item || Object.keys(store.item).length < 1)
-//         && route.params && route.params.id) {
-//
-//         await store.getItem(route.params.id);
-//     }
-//
-//     if(!store.list)
-//     {
-//         await store.getList();
-//     }
-//
-//     store.getSelectButtonOptions();
-//     store.getInventoryBulkMenu();
-//     store.watchSupplierName();
-//     store.watchInventoryStates();
-//
-//     if (store.quick_search_options?.length > 0) {
-//         store.selected_quick_search_options = store.quick_search_options[0].slug;
-//     }
-//
-// });
+    await store.getFormMenu();
+});
 
 //--------form_menu
 const form_menu = ref();
 const toggleFormMenu = (event) => {
     form_menu.value.toggle(event);
-
 };
-
 //--------/form_menu
 
 
@@ -87,216 +47,102 @@ watchEffect(() => {
 
     <div class="col-7" >
 
-        <Card class="is-small">
+        <Panel class="is-small">
+
+            <template class="p-1" #header>
 
 
-            <template #content>
-                <div v-if="store.item" class="mt-2">
-
-
-                    <div class="flex w-full justify-content-between mb-3">
-                        <div class="flex justify-content-center">
-
-                            <div class="ml-2">
-                                <h3>Shipments</h3>
-                                <h5>Create your shipment</h5>
-                            </div>
-                            <span v-if="store.item?.uuid" class="ml-1" style="font-size:15px;"><b>#{{store.item.uuid}}</b></span>
-                            <div >
-<!--                                <Chip label="Out "
-                                      style="margin-left:8px;height:25px;"
-                                      class="w-4rem bg-primary-100 border-primary-500"
-                                      input-class="text-primary-500"
-
-                                />-->
-<!--                                <Chip-->
-<!--                                      style="margin-left:8px;height:25px;"-->
-<!--                                      class="w-9rem bg-primary-100 border-primary-500"-->
-<!--                                      input-class="text-primary-500"-->
-<!--                                />-->
-                                <Dropdown
-                                    v-model="store.item.shipment_status"
-                                    :options="store.shipment_status"
-                                    optionLabel="name"
-                                    optionValue="id"
-                                    class="ml-2 w-7rem border-round-3xl border-primary-500 bg-primary-50"
-                                    input-class="text-primary-500 w-4rem px-2 py-1"
-                                    :pt="{
-                                        trigger: {
-                                            class: 'text-primary-500'
-                                        }
-                                    }"
-                                />
-
-                            </div>
-
-                        </div>
-                 <div>
-                     <div class="p-inputgroup">
-
-                         <Button class="p-button-sm"
-                                 v-tooltip.left="'View'"
-                                 v-if="store.item && store.item.id"
-                                 data-testid="categories-view_item"
-                                 @click="store.toView(store.item)"
-                                 icon="pi pi-eye"/>
-
-                         <Button label="Save"
-                                 class="p-button-sm"
-                                 v-if="store.item && store.item.id"
-                                 data-testid="categories-save"
-                                 @click="store.itemAction('save')"
-                                 icon="pi pi-save"/>
-
-                         <Button label="Create & New"
-                                 v-else
-                                 @click="store.itemAction('create-and-new')"
-                                 class="p-button-sm"
-                                 data-testid="categories-create-and-new"
-                                 icon="pi pi-save"/>
-
-
-                         <!--form_menu-->
-                         <Button
-                             type="button"
-                             @click="toggleFormMenu"
-                             class="p-button-sm"
-                             data-testid="categories-form-menu"
-                             icon="pi pi-angle-down"
-                             aria-haspopup="true"/>
-
-                         <Menu ref="form_menu"
-                               :model="store.form_menu_list"
-                               :popup="true"/>
-                         <!--/form_menu-->
-
-
-                         <Button class="p-button-primary p-button-sm"
-                                 icon="pi pi-times"
-                                 data-testid="categories-to-list"
-                                 @click="store.toList()">
-                         </Button>
-                     </div>
-                        </div>
+                <div class="flex flex-row">
+                    <div class="p-panel-title">
+                        <span v-if="store.item && store.item.id">
+                            Update
+                        </span>
+                        <span v-else>
+                            Create
+                        </span>
                     </div>
-                    <hr />
-                    <div class="flex w-full justify-content-between mt-4">
-                        <div class="supplier_details w-full ml-2">
-                            <Card class="h-full py-3 px-3" style="font-weight: 500;"
-                                  :pt="{
-                                        content: {
-                                            class: 'py-0'
-                                        }
-                                    }"
-                            >
-                                <template #content>
-                                    <div class="flex w-full justify-content-between">
-                                        <div class="flex">
-                                            <h4>Order Details</h4>
-<!--                                            <Chip-->
-<!--                                                :label="`Credit: `"-->
-<!--                                                style="margin-left:8px;height:20px"-->
-<!--                                                :pt="{-->
-<!--                                                root: {-->
-<!--                                                        style: {-->
-<!--                                                                background: 'lightblue'-->
-<!--                                                                }-->
-<!--                                                        }-->
-<!--                                                }"-->
-<!--                                            />-->
-                                        </div>
 
-                                    </div>
-                                    <div class="w-full mt-2">
+                </div>
 
-                                        <VhField label="Order">
-                                            <MultiSelect
-                                                v-model="store.item.orders"
-                                                :options="store.order_list"
-                                                filter
-                                                optionValue="id"
-                                                optionLabel="name"
-                                                placeholder="Select Order"
-                                                display="chip"
-                                                append-to="self"
-                                                class="w-full relative" />
-                                        </VhField>
-                                    </div>
 
-                                    <div v-if="store.item.orders?.length>0" class="w-full mt-2">
+            </template>
 
-                                        <VhField label="Order Items">
-                                            <MultiSelect
-                                                v-model="store.item.order_items"
-                                                :options="store.order_item_list"
-                                                filter
-                                                optionValue="id"
-                                                optionLabel="name"
-                                                placeholder="Select Order Items"
-                                                display="chip"
-                                                append-to="self"
-                                                class="w-full relative" />
-                                        </VhField>
-                                    </div>
-                                    <VhField label="Comment">
-                                            <Textarea rows="3" class="w-full"
-                                                      placeholder="Enter Comment"
-                                                      name="products-status_notes"
-                                                      data-testid="products-status_notes"
-                                                      v-model="store.item.status_notes"/>
-                                    </VhField>
+            <template #icons>
 
-                                </template>
-                            </Card>
+
+                <div class="p-inputgroup">
+
+                    <Button class="p-button-sm"
+                            v-tooltip.left="'View'"
+                            v-if="store.item && store.item.id"
+                            data-testid="shipments-view_item"
+                            @click="store.toView(store.item)"
+                            icon="pi pi-eye"/>
+
+                    <Button label="Save"
+                            class="p-button-sm"
+                            v-if="store.item && store.item.id"
+                            data-testid="shipments-save"
+                            @click="store.itemAction('save')"
+                            icon="pi pi-save"/>
+
+                    <Button label="Create & New"
+                            v-else
+                            @click="store.itemAction('create-and-new')"
+                            class="p-button-sm"
+                            data-testid="shipments-create-and-new"
+                            icon="pi pi-save"/>
+
+
+                    <!--form_menu-->
+                    <Button
+                        type="button"
+                        @click="toggleFormMenu"
+                        class="p-button-sm"
+                        data-testid="shipments-form-menu"
+                        icon="pi pi-angle-down"
+                        aria-haspopup="true"/>
+
+                    <Menu ref="form_menu"
+                          :model="store.form_menu_list"
+                          :popup="true" />
+                    <!--/form_menu-->
+
+
+                    <Button class="p-button-primary p-button-sm"
+                            icon="pi pi-times"
+                            data-testid="shipments-to-list"
+                            @click="store.toList()">
+                    </Button>
+                </div>
+
+
+
+            </template>
+
+
+            <div v-if="store.item" class="mt-2">
+
+                <Message severity="error"
+                         class="p-container-message mb-3"
+                         :closable="false"
+                         icon="pi pi-trash"
+                         v-if="store.item.deleted_at">
+
+                    <div class="flex align-items-center justify-content-between">
+
+                        <div class="">
+                            Deleted {{store.item.deleted_at}}
                         </div>
 
-
-                    </div>
-                    <hr class="mt-4" />
-
-                    <div v-if="store.item.order_items"  class="flex w-full justify-content-between mt-3">
-
-                        <div>
-                            <div class="p-inputgroup">
-
-                                <Button
-                                    type="button"
-                                    aria-haspopup="true"
+                        <div class="ml-3">
+                            <Button label="Restore"
                                     class="p-button-sm"
-                                    label="Delete"
-
-                                    :badge="store.selected_inventory?.length"
-                                    @click="store.removeInventoryItems()"
-                                />
-
-                                <!--bulk_menu-->
-                                <Button
-                                    type="button"
-                                    @click="toggleInventoryBulkMenu"
-                                    severity="primary" outlined
-                                    data-testid="companies-actions-bulk-menu"
-                                    aria-haspopup="true"
-                                    aria-controls="bulk_menu_state"
-                                    class="ml-1 p-button-sm">
-                                    <i class="pi pi-ellipsis-v"></i>
-                                </Button>
-
-                                <Menu ref="inventory_bulk_menu_state"
-                                      :model="store.inventory_bulk_menu"
-                                      :popup="true" />
-                                <!--/bulk_menu-->
-
-                            </div>
-
+                                    data-testid="articles-item-restore"
+                                    @click="store.itemAction('restore')">
+                            </Button>
                         </div>
 
-                        <div>
-                            <Button class="p-button-sm"
-                                    label="Add From Orders"
-                                    @click="store.showInventory()"
-                                    style="font-weight:600"
-                            />
-                        </div>
                     </div>
 
                 </Message>
@@ -401,6 +247,17 @@ watchEffect(() => {
                                         ></InputNumber>
                                     </div>
 
+                <VhField label="Select Order">
+                    <div class="flex justify-content-center">{{store.item.orders}}
+                        <AutoComplete
+                            v-model="store.item.orders"
+                            :suggestions="store.order_suggestion_list"
+                            multiple
+                            dropdown
+                            @complete="store.searchOrders($event)"
+                            optionLabel="name"
+                            placeholder="Select Order"
+                            display="chip"
 
                                 </template>
 
@@ -476,9 +333,5 @@ watchEffect(() => {
         </Panel>
 
     </div>
-
-
-
-
 
 </template>
