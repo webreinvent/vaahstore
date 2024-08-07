d40b6827984c91c61bac504ad9b3d5a0da84d813
Shivam Gupta <shivam-g001@webreinvent.com>
2024-08-07 19:06:55 +0530

    Added: modal component to edit shipment quantities

--- Vue/stores/store-shipments.js
+++ Vue/stores/store-shipments.js
@@ -1272,8 +1272,11 @@ export const useShipmentStore = defineStore({
         //---------------------------------------------------------------------
         openShippingQuantityModal(shipment_id){
             vaah().toastErrors(['This will impact quantity on other shipments as well.']);
-        }
+        },
         //---------------------------------------------------------------------
+        getShipmentItemRecords(){
+            // vaah().toastErrors(['This will impact quantity on other shipments as well.']);
+        }
     }
 });
 
