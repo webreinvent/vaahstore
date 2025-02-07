f2e444eaf5d43e82a207ad5bcdf7b1350117fd94
Shivam Gupta <shivam-g001@webreinvent.com>
2025-02-07 12:56:56 +0530

    Updated: store seeds

--- Database/Seeds/DatabaseTableSeeder.php
+++ Database/Seeds/DatabaseTableSeeder.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use VaahCms\Modules\Store\Models\Brand;
+use VaahCms\Modules\Store\Models\Currency;
+use VaahCms\Modules\Store\Models\Lingual;
 use VaahCms\Modules\Store\Models\Product;
 use VaahCms\Modules\Store\Models\Store;
 use VaahCms\Modules\Store\Models\Vendor;
 use WebReinvent\VaahCms\Entities\Taxonomy;
 use WebReinvent\VaahCms\Libraries\VaahSeeder;
+use Faker\Factory as Faker;
 
 class DatabaseTableSeeder extends Seeder
 {
@@ -37,6 +40,7 @@ class DatabaseTableSeeder extends Seeder
         $this->seedLanguages();
         $this->seedLanguageCategories();
         $this->seedLanguageStrings();
+        $this->seedStores();
 
 
         $seeder = new SettingTableSeeder();
@@ -174,6 +178,76 @@ class DatabaseTableSeeder extends Seeder
             }
         }
 
+    }
+    public function seedStores()
+    {
+        $faker = Faker::create();
+        $storeNames = [
+            'Green Valley Market', 'Urban Goods', 'FreshMart', 'Fresh Picked Produce',
+            'The Modern Shopper', 'Tech Haven', 'ElectroHub', 'The Book Nook',
+            'Chic Boutique', 'Nature’s Bounty', 'The Home Emporium', 'Tasteful Delights',
+            'Sports Gear Zone', 'Global Market', 'Style Loft', 'Vintage Finds',
+            'Fit & Active', 'Gadget Galaxy', 'Daily Essentials', 'City Grocery',
+            'Beauty Bliss', 'Luxury Living', 'Pets World', 'The Toy Chest',
+            'Health & Harmony', 'Outdoor Adventures', 'Happy Feet Shoes', 'Gourmet Eats',
+        ];
+
+        $statuses = Taxonomy::getTaxonomyByType('store-status')->pluck('id')->toArray();
+        $currencies = vh_st_get_country_currencies();
+        $languages = vh_st_get_country_languages();
+
+        $stores = [];
+        $currenciesToInsert = [];
+        $languagesToInsert = [];
+
+        for ($i = 0; $i < 1000; $i++) {
+            $store = new Store();
+            $store->name = $storeNames[array_rand($storeNames)];
+            $store->is_multi_currency = rand(0, 1);
+            $store->is_multi_lingual = rand(0, 1);
+            $store->is_multi_vendor = rand(0, 1);
+            $store->is_default = ($i === 0) ? 1 : 0;
+            $store->taxonomy_id_store_status = $statuses ? $statuses[array_rand($statuses)] : null;
+            $store->status_notes = 'store Status';
+            $store->is_active = rand(0, 1);
+            $store->slug = Str::slug($store->name . '-' . Str::random(5));
+            $store->allowed_ips = array_map(fn () => $faker->ipv4, range(1, 5));
+            $store->save();
+
+            // Multi-currency handling
+            if ($store->is_multi_currency && count($currencies) > 1) {
+                $randomCurrencies = array_rand($currencies, min(2, count($currencies)));
+                foreach ((array) $randomCurrencies as $index) {
+                    $currenciesToInsert[] = [
+                        'vh_st_store_id' => $store->id,
+                        'name' => $currencies[$index]['name'],
+                        'is_active' => 1,
+                    ];
+                }
+            }
+
+            // Multi-lingual handling
+            if ($store->is_multi_lingual && count($languages) > 1) {
+                $randomLanguages = array_rand($languages, min(2, count($languages)));
+                foreach ((array) $randomLanguages as $index) {
+                    $languagesToInsert[] = [
+                        'vh_st_store_id' => $store->id,
+                        'name' => $languages[$index]['name'],
+                        'is_active' => 1,
+                    ];
+                }
+            }
+        }
+
+        // Bulk insert currencies
+        if (!empty($currenciesToInsert)) {
+            Currency::insert($currenciesToInsert);
+        }
+
+        // Bulk insert languages
+        if (!empty($languagesToInsert)) {
+            Lingual::insert($languagesToInsert);
+        }
     }
 
 }
