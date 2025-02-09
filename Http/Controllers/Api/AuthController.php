1cce17a48fe1ad468db5d9bb14d1572cacce7b7a
Shivam Gupta <shivam-g001@webreinvent.com>
2025-02-09 13:28:22 +0530

    Updated: sign-in endpoint

--- Http/Controllers/Api/AuthController.php
+++ Http/Controllers/Api/AuthController.php
@@ -104,146 +104,7 @@ class AuthController  extends Controller
     //------------------------------------------------
 
 
-    public function authSignIn(Request $request)
-    {
-        try {
-            // Validation
-            $inputs = $request->all();
-            $rules = [
-                'email' => 'required|max:150',
-                'password' => 'required_without:type',
-                'type' => 'nullable|in:otp',
-                'login_otp' => 'required_if:type,otp',
-                'remember' => 'nullable|boolean',
-            ]; $messages = [
-                'email.required' => 'The email or username is required.',
-                'email.max' => 'The email field may not be greater than :max characters',
-                'password.required_without' => 'Password is required',
-                'login_otp.required_if' => 'OTP is required ',
-            ];
-            $validator = \Validator::make($inputs, $rules, $messages);
 
-            if ($validator->fails()) {
-                $errors = errorsToArray($validator->errors());
-                return response()->json([
-                    'success' => false,
-                    'errors' => $errors,
-                ], 422);
-            }
-
-            $user = \VaahCms\Modules\Store\Models\User::where('email', $request->email)
-                ->orWhere('username', $request->email)
-                ->first();
-
-            if (!$user) {
-                return response()->json([
-                    'success' => false,
-                    'errors' => ['Invalid credentials'],
-                ], 401);
-            }
-
-            // Handle OTP login
-            if ($request->type === 'otp') {
-                return $this->handleOtpLogin($user, $request);
-            }
-            // Handle without Otp login
-            return $this->handleStandardLogin($user, $request);
-
-        }catch (\Exception $e) {
-            $response = [];
-            $response['success'] = false;
-
-            if (env('APP_DEBUG')) {
-                $response['errors'][] = $e->getMessage();
-                $response['hint'] = $e->getTrace();
-            } else {
-                $response['errors'][] = trans("vaahcms-general.something_went_wrong");
-            }
-
-            return response()->json($response);
-        }
-    }
-    //------------------------------------------------
-
-    protected function handleOtpLogin($user, $request)
-    {
-        // Check OTP
-        if (Hash::check(trim($request->login_otp), $user->login_otp)) {
-            Auth::login($user, $request->boolean('remember'));
-
-            $max_sessions = 5;
-            if ($user->tokens()->count() >= $max_sessions) {
-                $user->tokens()->oldest()->first()->delete();
-            }
-
-            $expiration = Carbon::now()->addHours(24);
-
-            $token = $user->createToken('VaahStore')->plainTextToken;
-
-            $user->tokens()->latest()->first()->update(['expires_at' => $expiration]);
-
-            $user->update([
-                'login_otp' => null,
-                'last_login_at' => now(),
-                'last_login_ip' => $request->ip(),
-            ]);
-
-            return response()->json([
-                'success' => true,
-                'data' => [
-                    'item' => array_merge($user->toArray(), [
-                        'api_token' => $token,
-                        'expires_at' => $expiration->toDateTimeString(),
-                    ]),
-                ],
-            ]);
-        }
-
-        return response()->json([
-            'success' => false,
-            'errors' => ['The OTP you entered is invalid.'],
-        ], 401);
-    }
-    //------------------------------------------------
-
-    protected function handleStandardLogin($user, $request)
-    {
-        if (Hash::check($request->password, $user->password)) {
-
-            $login_response = User::login($request);
-
-            // If login failed
-            if (!empty($login_response['status']) && $login_response['status'] === 'failed') {
-                return response()->json($login_response,401);
-            }
-
-            // Set maximum session limit
-            $max_sessions = 5;
-
-            if ($user->tokens()->count() >= $max_sessions) {
-                $user->tokens()->oldest()->first()->delete();
-            }
-            $expiration = Carbon::now()->addHours(24);
-
-            $token = $user->createToken('VaahStore')->plainTextToken;
-
-            $user->tokens()->latest()->first()->update(['expires_at' => $expiration]);
-
-            return response()->json([
-                'success' => true,
-                'data' => [
-                    'item' => array_merge($user->toArray(), [
-                        'api_token' => $token,
-                        'expires_at' => $expiration->toDateTimeString(),
-                    ]),
-                ],
-            ]);
-
-        }
-        return response()->json([
-            'success' => false,
-            'errors' => ['The password you entered is incorrect.'],
-        ], 401);    }
 
     //------------------------------------------------
 
@@ -324,4 +185,167 @@ class AuthController  extends Controller
             return response()->json($response, 500);
         }
     }
+
+    public function authSignIn(Request $request)
+    {
+        try {
+            $request->merge([
+                'identifier_key' => $request->identifier_key ?? 'email',
+                'authentication_type' => $request->authentication_type ?? 'password',
+            ]);
+            $validator = self::validateLoginRequest($request);
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'errors' => $validator->errors()->all(),
+                ], 422);
+            }
+
+            $user = self::findUser($request);
+            if (!$user) {
+                return response()->json([
+                    'success' => false,
+                    'errors' => ['Invalid credentials'],
+                ], 401);
+            }
+
+            if ($request->authentication_type === 'otp') {
+                return self::handleOtpLogin($user, $request);
+            }
+
+            return self::handleStandardLogin($user, $request);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'errors' => [env('APP_DEBUG') ? $e->getMessage() : trans('vaahcms-general.something_went_wrong')],
+                'hint' => env('APP_DEBUG') ? $e->getTrace() : null,
+            ]);
+        }
+    }
+
+    protected static function validateLoginRequest(Request $request)
+    {
+        $rules = [
+            'identifier_key' => 'required|in:email,username,phone',
+            'identifier_value' => 'required',
+            'authentication_type' => 'required|in:password,otp',
+            'authentication_value' => 'required|string',
+            'remember' => 'nullable|boolean',
+        ];
+        if ($request->identifier_key === 'phone') {
+            $rules['identifier_value'] .= '|numeric';
+        }
+        $messages = [
+            'identifier_key.required' => 'Identifier key is required.',
+            'identifier_key.in' => 'The selected identifier key is invalid. It must be one of: email, username, phone.',
+            'identifier_value.required' => 'Identifier value is required.',
+            'authentication_type.required' => 'Authentication type is required.',
+            'authentication_value.required' => 'Authentication value is required.',
+
+            'identifier_value.phone' => 'The phone number format is invalid.',
+            'identifier_value.numeric' => 'The phone number must be a valid numeric value.',
+        ];
+
+        return \Validator::make($request->all(), $rules, $messages);
+    }
+
+    protected static function findUser(Request $request)
+    {
+        $identifier_key = $request->identifier_key;
+        $identifierV_value = $request->identifier_value;
+
+        switch ($identifier_key) {
+
+            case 'email':
+                return \VaahCms\Modules\Store\Models\User::where('email', $identifierV_value)->first();
+            case 'username':
+                return \VaahCms\Modules\Store\Models\User::where('username', $identifierV_value)->first();
+            case 'phone':
+                if (is_numeric($identifierV_value)) {
+                    return \VaahCms\Modules\Store\Models\User::where('phone', $identifierV_value)->first();
+                }
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    protected static function handleStandardLogin($user, $request)
+    {
+        if (Hash::check($request->authentication_value, $user->password)) {
+            return self::generateAuthResponse($user, $request);
+        }
+
+        return response()->json([
+            'success' => false,
+            'errors' => ['The password you entered is incorrect.'],
+        ], 401);
+    }
+
+    protected static function generateAuthResponse($user, $request)
+    {
+        $max_sessions = 5;
+        if ($user->tokens()->count() >= $max_sessions) {
+            $user->tokens()->oldest()->first()->delete();
+        }
+
+        $expiration = Carbon::now()->addHours(24);
+
+        $token = $user->createToken('VaahStore')->plainTextToken;
+        $user->tokens()->latest()->first()->update(['expires_at' => $expiration]);
+
+        $user->makeVisible('api_token');
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'item' => array_merge($user->toArray(), [
+                    'api_token' => $token,
+                    'expires_at' => $expiration->toDateTimeString(),
+                ]),
+            ],
+        ]);
+    }
+
+    protected static function handleOtpLogin($user, $request)
+    {
+        if (Hash::check(trim($request->authentication_value), $user->login_otp)) {
+            Auth::login($user, $request->boolean('remember'));
+
+            $max_sessions = 5;
+            if ($user->tokens()->count() >= $max_sessions) {
+                $user->tokens()->oldest()->first()->delete();
+            }
+
+            $expiration = Carbon::now()->addHours(24);
+
+            $token = $user->createToken('VaahStore')->plainTextToken;
+
+            $user->tokens()->latest()->first()->update(['expires_at' => $expiration]);
+
+            $user->update([
+                'login_otp' => null,
+                'last_login_at' => now(),
+                'last_login_ip' => $request->ip(),
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'item' => array_merge($user->toArray(), [
+                        'api_token' => $token,
+                        'expires_at' => $expiration->toDateTimeString(),
+                    ]),
+                ],
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'errors' => ['The OTP you entered is invalid.'],
+        ], 401);
+    }
+
+
 }
