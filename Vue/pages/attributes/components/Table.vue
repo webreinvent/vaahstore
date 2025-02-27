fb0f1052b9a5d30dad4b52f719d813e82b51b574
Atul Pratap Singh <atul-s001@webreinvent.com>
2025-02-27 12:31:43 +0530

    Updated: Table and Item

--- Vue/pages/attributes/components/Table.vue
+++ Vue/pages/attributes/components/Table.vue
@@ -20,7 +20,7 @@ const useVaah = vaah();
                    responsiveLayout="scroll">
 
             <Column selectionMode="multiple"
-                    v-if="store.isViewLarge()"
+                    v-if="store.isListView()"
                     headerStyle="width: 3em">
             </Column>
 
@@ -40,29 +40,29 @@ const useVaah = vaah();
             </Column>
 
                 <Column field="updated_at" header="Updated"
-                        v-if="store.isViewLarge()"
+                        v-if="store.isListView()"
                         style="width:150px;"
                         :sortable="true">
 
                     <template #body="prop">
-                        {{useVaah.ago(prop.data.updated_at)}}
+                        {{useVaah.toLocalTimeShortFormat(prop.data.updated_at)}}
                     </template>
 
                 </Column>
 
-            <Column field="is_active" v-if="store.isViewLarge()"
+            <Column field="is_active" v-if="store.isListView()"
 
                     style="width:100px;"
                     header="Is Active">
 
                 <template #body="prop">
-                    <InputSwitch v-model.bool="prop.data.is_active"
+                    <ToggleSwitch v-model.bool="prop.data.is_active"
                                  :disabled="!store.assets.permissions.includes('can-update-module')"
                                  data-testid="attributes-table-is-active"
                                  v-bind:false-value="0"  v-bind:true-value="1"
                                  class="p-inputswitch-sm"
                                  @input="store.toggleIsActive(prop.data)">
-                    </InputSwitch>
+                    </ToggleSwitch>
                 </template>
 
             </Column>
@@ -91,7 +91,7 @@ const useVaah = vaah();
 
                         <Button class="p-button-tiny p-button-danger p-button-text"
                                 data-testid="attributes-table-action-trash"
-                                v-if="store.isViewLarge() && !prop.data.deleted_at &&store.assets.permissions.includes('can-update-module')"
+                                v-if="store.isListView() && !prop.data.deleted_at &&store.assets.permissions.includes('can-update-module')"
                                 @click="store.itemAction('trash', prop.data)"
                                 v-tooltip.top="'Trash'"
                                 icon="pi pi-trash" />
@@ -99,7 +99,7 @@ const useVaah = vaah();
 
                         <Button class="p-button-tiny p-button-success p-button-text"
                                 data-testid="attributes-table-action-restore"
-                                v-if="store.isViewLarge() && prop.data.deleted_at &&  store.assets.permissions.includes('can-update-module')"
+                                v-if="store.isListView() && prop.data.deleted_at &&  store.assets.permissions.includes('can-update-module')"
                                 @click="store.itemAction('restore', prop.data)"
                                 v-tooltip.top="'Restore'"
                                 icon="pi pi-replay" />
